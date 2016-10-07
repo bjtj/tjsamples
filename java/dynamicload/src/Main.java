ed68c9db8f870b9a54ae80dd0dc8e397efbef2dc
bjtj <bjtj10@gmail.com>
2016-10-07 16:34:56 +0900

    static block called when loading time

--- java/dynamicload/src/Main.java
+++ java/dynamicload/src/Main.java
@@ -1,8 +1,13 @@
 import java.lang.reflect.*;
 
 class Main {
-	public static void main(String args[]) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+	public static void main(String args[]) throws ClassNotFoundException,
+		NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+
+		System.out.println("-- loading com.myapp.Hello : begin --");
 		Class<?> cls = Class.forName("com.myapp.Hello");
+		System.out.println("-- loading com.myapp.Hello : done --");
+		
 		System.out.println("Class : " + cls.getName());
 		System.out.println("Package : " + cls.getPackage());
 
--- java/dynamicload/src/com/myapp/Hello.java
+++ java/dynamicload/src/com/myapp/Hello.java
@@ -1,6 +1,11 @@
 package com.myapp;
 
 public class Hello {
+
+	static {
+		System.out.println(Hello.class.getName() + " loaded");
+	}
+	
 	public void hello() {
 		System.out.println("Hello World!");
 	}
