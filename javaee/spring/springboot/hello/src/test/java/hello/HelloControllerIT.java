bed84d309f9c0e445bddc20ffc419aa13b16eef7
bjtj <bjtj10@gmail.com>
2017-01-18 15:25:10 +0900

    javaee spring boot sample

---
+++ javaee/spring/springboot/hello/src/test/java/hello/HelloControllerIT.java
@@ -0,0 +1,43 @@
+package hello;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Before;
+import org.junit.Test;
+import java.net.URL;
+import org.junit.runner.RunWith;
+import org.junit.Assert;
+import org.hamcrest.Matchers;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerIT {
+	
+	@LocalServerPort
+	private int port;
+
+	private URL base;
+
+	@Autowired
+	private TestRestTemplate template;
+
+	@Before
+	public void setUp() throws Exception {
+		this.base = new URL("http://localhost:" + port + "/");
+	}
+
+	@Test
+	public void getHello() throws Exception {
+		ResponseEntity<String> response = template.getForEntity(base.toString(),
+																String.class);
+		assertThat(response.getBody(), equalTo("Greetings from Sprng Boot!"));
+	}
+}
