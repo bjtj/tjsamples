69c1d267eb0109c465e44063aeec6fab06ebd5fc
bjtj <bjtj10@gmail.com>
2017-12-15 08:48:18 +0900

    ignore list updated & http get print url

--- java/http/src/HttpGet.java
+++ java/http/src/HttpGet.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 class HttpGet {
 	public static void main(String args[]) throws IOException {
+		System.out.printf("[Download: %s]%n", args[0]);
 		URL url = new URL(args[0]);
 		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
 		InputStream in = conn.getInputStream();
