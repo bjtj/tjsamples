b4878e49c184fafe18bef97884c97e25ba105291
bjtj <bjtj10@gmail.com>
2018-06-11 07:53:35 +0900

    swift xml

--- swift/xml/Sources/xml/main.swift
+++ swift/xml/Sources/xml/main.swift
@@ -1,16 +1,36 @@
 import Foundation
 
-// https://www.w3schools.com/xml/note.xml
-let xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-  "<note>" +
-  "  <to>Tove</to>" +
-  "  <from>Jani</from>" +
-  "  <heading>Reminder</heading>" +
-  "  <body>Don't forget me this weekend!</body>" +
-  "</note>"
+func test() throws {
 
-let dom = try XMLDocument(xmlString: xml)
+    // https://www.w3schools.com/xml/note.xml
+    let xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+      "<note>" +
+      "  <to>Tove</to>" +
+      "  <from>Jani</from>" +
+      "  <heading>Reminder</heading>" +
+      "  <body>Don't forget me this weekend!</body>" +
+      "</note>"
 
-// let dom = try XMLDocument(contentsOf: URL(string: "https://www.w3schools.com/xml/note.xml")!)
+    let dom = try XMLDocument(xmlString: xml)
+    
+    print(dom.rootElement()!)
+}
 
-print(dom.rootElement()!)
+func test2() throws {
+    let url = URL(string: "https://www.w3schools.com/xml/note.xml")
+    print("url: \(url!)")
+    let dom = try XMLDocument(contentsOf: url!)
+    
+    print(dom.rootElement()!)
+}
+
+do {
+    try test()
+} catch {
+    print("fail")
+}
+do {
+    try test2()
+} catch {
+    print("fail")
+}
