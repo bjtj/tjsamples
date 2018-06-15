bf74860b7fb57dd8822d4fef032336c52d1a2380
bjtj <bjtj10@gmail.com>
2018-06-15 17:34:50 +0900

    nodejs - sequelize sample

---
+++ nodejs/tut/sequelize/README.md
@@ -0,0 +1,26 @@
+http://docs.sequelizejs.com/
+
+Getting Started
+====
+
+http://docs.sequelizejs.com/manual/installation/getting-started
+
+```
+// Using NPM
+$ npm install --save sequelize
+
+# And one of the following:
+$ npm install --save pg pg-hstore
+$ npm install --save mysql2
+$ npm install --save sqlite3
+$ npm install --save tedious // MSSQL
+
+// Using Yarn
+$ yarn add sequelize
+
+# And one of the following:
+$ yarn add pg pg-hstore
+$ yarn add mysql2
+$ yarn add sqlite3
+$ yarn add tedious // MSSQL
+```
---
+++ nodejs/tut/sequelize/app.js
@@ -0,0 +1,49 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('mysql://root:test123@localhost/mydb')
+
+// https://www.npmjs.com/package/system-sleep
+var sleep = require('system-sleep')
+
+
+// Test the connection
+sequelize.authenticate()
+    .then(() => {
+	console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+	console.log('Unable to connect to the database:', err);
+    });
+
+
+// Your first model
+console.log('[define]')
+const User = sequelize.define('user', {
+    firstName: {
+	type: Sequelize.STRING
+    },
+    lastName: {
+	type: Sequelize.STRING
+    }
+});
+
+console.log('[sync]')
+User.sync({force: true}).then(()=> {
+    return User.create({
+	firstName: 'John',
+	lastName: 'Hancock'
+    });
+});
+
+
+// Your first query
+
+sleep(1000)
+
+console.log('[find all]')
+User.findAll().then(users => {
+    console.log(users)
+});
+
+sleep(1000)
+
+process.exit()
