23a34c91b1a850548a2928c63fb40b500a58c42d
bjtj <bjtj10@gmail.com>
2018-08-31 10:53:45 +0900

    c date time

---
+++ c/date/README.md
@@ -0,0 +1,3 @@
+# stftime()
+
+* https://www.ibm.com/support/knowledgecenter/ko/ssw_ibm_i_73/rtref/strfti.htm
---
+++ c/date/main.c
@@ -0,0 +1,58 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+
+    char buffer[1024] = {0,};
+    time_t ltime;
+    struct tm * timeptr;
+    char * old_locale;
+    char * saved_locale;
+    const char * date_fmt = "%Y-%m-%d %H:%M:%S %Z [%x %X (%p)]";
+
+    /* time() */
+    ltime = time(NULL);
+
+    /*
+     * ctime()
+     * * https://www.ibm.com/support/knowledgecenter/ko/ssw_ibm_i_73/rtref/ctime.htm
+     *
+     * ctime always returns date in English
+     * * https://stackoverflow.com/a/33118886
+     * */
+    printf("Time: %s", ctime(&ltime));
+
+
+    /*
+     * strftime()
+     * * https://www.ibm.com/support/knowledgecenter/ko/ssw_ibm_i_73/rtref/strfti.htm
+     * */
+    timeptr = localtime(&ltime);
+    strftime(buffer, sizeof(buffer), date_fmt, timeptr);
+    printf("Time: %s\n", buffer);
+
+
+    /*
+     * setlocale()
+     * * https://www.gnu.org/software/libc/manual/html_node/Setting-the-Locale.html
+     * */
+    old_locale = setlocale(LC_ALL, NULL);
+    saved_locale = strdup(old_locale);
+    if (setlocale(LC_ALL, "ko_KR.UTF8") == NULL) {
+	fprintf(stderr, "Unable to set locale\n");
+    }
+
+    strftime(buffer, sizeof(buffer), date_fmt, timeptr);
+    printf("Time: %s\n", buffer);
+
+    setlocale(LC_ALL, saved_locale);
+    free(saved_locale);
+    
+    
+    return 0;
+}
