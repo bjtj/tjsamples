884afacef823c37c50c554091039ca6aa1b12902
bjtj <bjtj10@gmail.com>
2018-09-01 15:25:09 +0900

    python string example

---
+++ python/string/main.py
@@ -0,0 +1,31 @@
+
+
+def main():
+    # contains
+    print('llo' in 'hello')
+    # upper
+    print('heLlo'.upper())
+    # lower
+    print('hEllo'.lower())
+    # capital
+    print('hEllo World'.title())
+    # trim
+    print('   ab c  '.strip())
+    # split
+    print('a,b,c'.split(','))
+    # splitlines
+    print('line1\nline2\nline3\n'.splitlines())
+    # empty
+    if not '':
+        print('(empty)')
+    # substring
+    print('hello world'[3:7])
+    # reverse
+    print('hello world'[::-1])
+    # length
+    print(len('hello world'))
+    # unicode
+    print(u'X')
+
+if __name__ == '__main__':
+    main()
