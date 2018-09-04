95fd2c116db36d88bcb3f90688a9c1e38fa17cae
bjtj <bjtj10@gmail.com>
2018-09-04 23:15:36 +0900

    ruby socket SO_REUSEADDR

--- ruby/socket/multicast/recv.rb
+++ ruby/socket/multicast/recv.rb
@@ -9,11 +9,15 @@ PORT = 1900
 socket = UDPSocket.new
 membership = IPAddr.new(MULTICAST_ADDR).hton + IPAddr.new(BIND_ADDR).hton
 socket.setsockopt(:IPPROTO_IP, :IP_ADD_MEMBERSHIP, membership)
+socket.setsockopt(:SOL_SOCKET, :SO_REUSEADDR, 1)
 socket.setsockopt(:SOL_SOCKET, :SO_REUSEPORT, 1)
 socket.bind(BIND_ADDR, PORT)
 
+fds = [socket]
 
 loop do
-  msg, _ = socket.recvfrom(1024)
-  puts msg
+  if ios = select(fds, [], [], 1)
+    msg, _ = socket.recvfrom(1024)
+    puts msg
+  end
 end
