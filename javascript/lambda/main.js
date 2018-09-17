649bc0958be9fa62054e85a8c9d58771644fca4f
bjtj <bjtj10@gmail.com>
2018-09-17 11:40:02 +0900

    javascript lambda example

---
+++ javascript/lambda/main.js
@@ -0,0 +1,13 @@
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Functions/%EC%95%A0%EB%A1%9C%EC%9A%B0_%ED%8E%91%EC%85%98
+
+var materials = [
+    'Hydrogen', 'Helium', 'Lithium', 'Beryllium'
+];
+
+// identical
+console.log(materials.map(material => material));
+
+// length
+console.log(materials.map(material => material.length));
+
+
