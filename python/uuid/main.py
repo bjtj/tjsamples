0775393a50b4b96213e1ae791924b0b3d1c85afc
bjtj <bjtj10@gmail.com>
2018-09-17 11:31:02 +0900

    python uuid example

---
+++ python/uuid/main.py
@@ -0,0 +1,19 @@
+# https://docs.python.org/2/library/uuid.html
+
+import uuid
+
+
+def main():
+    print(uuid.uuid1())
+    print(uuid.uuid3(uuid.NAMESPACE_DNS, 'python.org'))
+    print(uuid.uuid4())
+    print(uuid.uuid5(uuid.NAMESPACE_DNS, 'python.org'))
+    val = uuid.UUID('{00010203-0405-0607-0809-0a0b0c0d0e0f}')
+    print(val)
+    print(val.hex)
+    print(val.urn)
+    print(uuid.UUID(bytes=val.bytes))
+    
+
+if __name__ == '__main__':
+    main()
