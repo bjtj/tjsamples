7f59e3fbce4d607fd5e085e2dcfc121fb6c0886b
bjtj <bjtj10@gmail.com>
2018-09-17 11:40:09 +0900

    javascript class example

---
+++ javascript/class/main.js
@@ -0,0 +1,20 @@
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Object/toString
+
+
+function Dog(name, breed, color, sex) {
+    this.name = name;
+    this.breed = breed;
+    this.color = color;
+    this.sex = sex;
+}
+
+var theDog = new Dog('Gabby', 'Lab', 'chocolate', 'female');
+
+console.log(theDog.toString());
+
+Dog.prototype.toString = function() {
+    var ret = 'Dog ' + this.name + ' is a ' + this.sex + ' ' + this.color + ' ' + this.breed;
+    return ret;
+}
+
+console.log(theDog.toString());
