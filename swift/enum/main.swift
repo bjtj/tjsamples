906ef714dfa582f0fe446d5288b0ac412a7cf7ca
bjtj <bjtj10@gmail.com>
2018-09-19 13:11:50 +0900

    swift string to enum example

--- swift/enum/main.swift
+++ swift/enum/main.swift
@@ -17,3 +17,22 @@ print(NTS.Alive)
 
 let nts = NTS.Update
 print(nts)
+
+
+// https://stackoverflow.com/a/38208335/5676460
+
+enum Month : String {
+    case jan = "January"
+    case feb = "February"
+    case mar = "March"
+}
+
+var month = Month(rawValue: "January")
+print(month == Month.jan)
+
+month = Month(rawValue: "February")
+print(month == Month.feb)
+
+month = Month(rawValue: "no")
+print(month == nil)
+print(month ?? "nil")
