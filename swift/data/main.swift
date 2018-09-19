06bd8ba53df0fa7641832739cce4c9896c967750
bjtj <bjtj10@gmail.com>
2018-09-19 16:59:52 +0900

    swift data example

--- swift/data/main.swift
+++ swift/data/main.swift
@@ -15,3 +15,9 @@ for d in data! {
 print("\(data!.subdata(in: 0..<2).map {"\($0)"} )")
 
 print("\(data!.subdata(in: 2..<7).map {"\($0)"} )")
+
+print(String(data: data!.subdata(in: 2..<7), encoding: .utf8)!)
+
+print("line1\r\nline2\r\n".data(using: .utf8)!.range(of: "\r\n".data(using: .utf8)!))
+
+print("line1\r\nline2\r\n".data(using: .utf8)!.range(of: "xx".data(using: .utf8)!))
