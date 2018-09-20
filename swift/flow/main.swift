038170b20ade766873c56894df4b09cbc28d396b
bjtj <bjtj10@gmail.com>
2018-09-20 11:11:49 +0900

    swift flow -- for loop example

---
+++ swift/flow/main.swift
@@ -0,0 +1,15 @@
+
+for i in 1...5 {
+    print(i, terminator: "")
+}
+print()
+
+for i in 0...5 {
+    print(i, terminator: "")
+}
+print()
+
+for i in 0..<5 {
+    print(i, terminator: "")
+}
+print()
