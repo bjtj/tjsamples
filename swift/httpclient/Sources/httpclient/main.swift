a7b3d878c1a9510204971b25770f41946d4e8eb7
bjtj <bjtj10@gmail.com>
2018-09-27 09:34:00 +0900

    swift http request post example

--- swift/httpclient/Sources/httpclient/main.swift
+++ swift/httpclient/Sources/httpclient/main.swift
@@ -1,38 +1,72 @@
 import Foundation
 
-let config = URLSessionConfiguration.default
-let session = URLSession(configuration: config)
-let req = URLRequest(url: URL(string: "http://example.com")!)
-let res: URLResponse? = nil
+func doGet() {
+    let config = URLSessionConfiguration.default
+    let session = URLSession(configuration: config)
+    let req = URLRequest(url: URL(string: "http://example.com")!)
+    let task = session.dataTask(with: req) {
+        (data, response, error) in
+        guard error == nil else {
+            print("error: \(error!)")
+            return
+        }
 
-let task = session.dataTask(with: req) {
-    (data, response, error) in
-    guard error == nil else {
-        print("error: \(error!)")
-        return
-    }
+        guard let responseData = data else {
+            print("error: no response data")
+            return
+        }
 
-    guard let responseData = data else {
-        print("error: no response data")
-        return
-    }
-
-    do {
-        print(responseData)
-        if let str = String(data: responseData, encoding: .utf8) {
-            print(str)
-        } else {
-            print("invalid encoding")
+        do {
+            print(responseData)
+            if let str = String(data: responseData, encoding: .utf8) {
+                print(str)
+            } else {
+                print("invalid encoding")
+            }
         }
     }
+    task.resume()
 }
-
-task.resume()
+doGet()
 
 sleep(1)
 
 print("----")
 
+func doPost() {
+    let config = URLSessionConfiguration.default
+    let session = URLSession(configuration: config)
+    var req = URLRequest(url: URL(string: "http://httpbin.org/post")!)
+    req.httpMethod = "POST"
+    req.httpBody = "hello".data(using: .utf8)
+    req.addValue("text/plain", forHTTPHeaderField: "Content-Type")
+    
+    let task = session.dataTask(with: req) {
+        (data, response, error) in
+        guard error == nil else {
+            print("error: \(error!)")
+            return
+        }
+
+        guard let responseData = data else {
+            print("error: no response data")
+            return
+        }
+
+        do {
+            print(responseData)
+            if let str = String(data: responseData, encoding: .utf8) {
+                print(str)
+            } else {
+                print("invalid encoding")
+            }
+        }
+    }
+    task.resume()
+}
+doPost()
+
+sleep(1)
 
 // url
 // - http://www.codingexplorer.com/creating-and-modifying-nsurl-in-swift/
