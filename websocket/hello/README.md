1a0d0c072e0bd17a965701e7c5c3403dc686de51
bjtj <bjtj10@gmail.com>
2018-10-11 17:11:13 +0900

    websocket hello example

---
+++ websocket/hello/README.md
@@ -0,0 +1,3 @@
+# WebSocket hello world in Javascript
+
+* http://scripting.com/2015/11/06/websocketsHelloWorld.html
---
+++ websocket/hello/server.js
@@ -0,0 +1,14 @@
+// https://gist.github.com/scripting/59498807e081b876a2b6
+
+var ws = require("nodejs-websocket");
+var port = 1337;
+
+function handleConnection(conn) {
+	conn.on("text", function (s) {
+		var str = s.toUpperCase() + "!!!";
+		console.log("handleConnection: returning " + str);
+		conn.sendText(str);
+	});
+}
+
+ws.createServer(handleConnection).listen(port);
