daba4ed2da4645858c801770a36a3221e49a5646
bjtj <bjtj10@gmail.com>
2018-10-24 11:39:17 +0900

    c env -- win32 example added

--- c/env/main.c
+++ c/env/main.c
@@ -1,3 +1,36 @@
+#ifdef _WIN32
+
+#include <Windows.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    LPTSTR lpszVariable;
+    LPTCH lpvEnv;
+    lpvEnv = GetEnvironmentStrings();
+    if (lpvEnv == NULL)
+    {
+	fprintf(stderr, "GetEnvironmentStrings() failed\n");
+	exit(1);
+    }
+    for (lpszVariable = (LPTSTR)lpvEnv; *lpszVariable; lpszVariable++)
+    {
+	while (*lpszVariable)
+	    putchar(*lpszVariable++);
+	putchar('\n');
+    }
+
+    if(FreeEnvironmentStrings(lpvEnv) == 0)
+	printf("GetEnvironmentStrings() failed.\n");
+    else
+	printf("\nGetEnvironmentStrings() is OK.\n");
+    
+    return 0;
+}
+
+
+#else
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,3 +50,5 @@ int main(int argc, char *argv[])
     
     return 0;
 }
+
+#endif
