285f02a8ebfc7f1159179e7360c0dbe983414b5e
bjtj <bjtj10@gmail.com>
2018-11-01 17:15:46 +0900

    added: javaee paraya netbeans hello example (stateless session bean)

---
+++ javaee/paraya/hello/JavaLibrary1/src/com/tjapp/ejb/hello/HelloSessionBeanRemote.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tjapp.ejb.hello;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author tjjang
+ */
+@Remote
+public interface HelloSessionBeanRemote {
+
+    String getGreeting();
+    
+}
