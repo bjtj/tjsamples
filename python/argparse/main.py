224c9536f28b5e154e8411b41da7587eaaf4076e
bjtj <bjtj10@gmail.com>
2018-11-06 18:14:47 +0900

    python argparse choice

--- python/argparse/main.py
+++ python/argparse/main.py
@@ -7,6 +7,7 @@ def main():
     parser.add_argument('--name', type=str, default='hello', help='name value')
     parser.add_argument('--count', type=int, default=0, help='count value')
     parser.add_argument('--debug', action='store_true', help='debug mode flag')
+    parser.add_argument('--choice', default='c', choices=['a', 'b', 'c'], help='choise one of (a b c)')
     args = parser.parse_args()
     print(args)
     args, unknown = parser.parse_known_args()
