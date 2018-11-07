0ae210713866b50b8febe1b6b549e4668e82ab5d
bjtj <bjtj10@gmail.com>
2018-11-07 13:09:35 +0900

    javascript functional programming example

---
+++ javascript/functional/main.js
@@ -0,0 +1,63 @@
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Functions/%EC%95%A0%EB%A1%9C%EC%9A%B0_%ED%8E%91%EC%85%98
+
+var materials = [
+    'Hydrogen', 'Helium', 'Lithium', 'Beryllium'
+];
+
+
+// ---
+// map
+// ---
+
+
+// identical
+console.log(materials.map(material => material));
+
+// length
+console.log(materials.map(material => material.length));
+
+
+// -------
+// forEach
+// -------
+
+var ret = materials.forEach(function (e) {
+    console.log(e.toUpperCase());
+});
+
+console.log(ret);		// undefined
+
+
+// ------
+// filter
+// ------
+
+var ret = materials.filter(e => e[0] == 'H');
+console.log(ret);
+var ret = materials.filter(e => e[0] != 'H');
+console.log(ret);
+console.log(materials);
+
+
+// -------
+// closure
+// -------
+
+
+function makeClosure(i) {
+    return function() {
+	i++;
+	console.log(i);
+	return i;
+    };
+}
+
+var closure = makeClosure(0);
+closure();
+closure();
+closure();
+
+var closure = makeClosure(0);
+closure();
+closure();
+closure();
