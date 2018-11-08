13161b217187689c6a11496804cad7ddaad4b267
bjtj <bjtj10@gmail.com>
2018-11-08 14:22:37 +0900

    added: websocket -- flask socketio example

---
+++ websocket/flask_socketio/README.md
@@ -0,0 +1,14 @@
+# flask socketio
+
+* https://flask-socketio.readthedocs.io/en/latest/
+
+```
+pip install -r requirements.txt
+python server.py
+```
+
+* open the browser
+
+```
+http://localhost:50000/
+```
---
+++ websocket/flask_socketio/server.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
+from threading import Event, Thread
+import time
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('message')
+def handle_message(msg):
+    print('msg: {}'.format(msg))
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('custom event msg: {}'.format(json))
+
+
+class TimerThread(Thread):
+    def __init__(self, interval, func):
+        Thread.__init__(self)
+        self.event = Event()
+        self.interval = interval
+        self.func = func
+        self.finishing = False
+
+    def run(self):
+        while self.finishing == False:
+            time.sleep(self.interval)
+            self.func()
+
+    def finish(self):
+        self.finishing = True
+
+def send_hello():
+    print('[{}] send hello'.format(time.time()))
+    socketio.emit('my response', 'hello')
+
+def main():
+    t = TimerThread(1, send_hello)
+    t.daemon = True
+    t.start()
+    socketio.run(app, port=50000)
+
+
+if __name__ == '__main__':
+    main()
