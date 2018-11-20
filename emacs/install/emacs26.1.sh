0b19c2bf592595289a9fc96e159570c955dbf011
bjtj <bjtj10@gmail.com>
2018-11-20 17:08:05 +0900

    edit: emacs build script

--- emacs/install/emacs26.1.sh
+++ emacs/install/emacs26.1.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+mkdir -p /tmp/emacs
+cd /tmp/eamcs
 sudo apt install libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgnutls-dev libncurses-dev
 curl -OL http://ftp.jaist.ac.jp/pub/GNU/emacs/emacs-26.1.tar.gz
 tar xvf emacs-26.1.tar.gz
-cd emacs-26.1.tar.gz
+cd emacs-26.1
 ./configure --with-x-toolkit=no
 make -j
 sudo make install
+rm -rf /tmp/emacs
