a281f0ea462a8becf5a70fd6ce253523ba12d99e
bjtj <bjtj10@gmail.com>
2018-11-21 11:34:14 +0900

    update: shell -- check if var set

--- shell/variable/main.sh
+++ shell/variable/main.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+# set -e # stop on fail
+
+check_if_set()
+{
+    # check if set
+    # - https://stackoverflow.com/a/13864829/5676460
+    if [ -z ${VAR+x} ]; then
+	echo "VAR is unset"
+    else
+	echo "VAR is set to '$VAR'";
+    fi
+}
 
 VAR=hello world # fail
-echo $VAR
+echo "\$VAR = '$VAR'"
+
+check_if_set
 
 VAR="hello world"
-echo $VAR
+echo "\$VAR = '$VAR'"
+
+check_if_set
