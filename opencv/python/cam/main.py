c3ff5753b20075da492dc6f174aec0dd3d8490eb
bjtj <bjtj10@gmail.com>
2018-11-30 10:39:52 +0900

    update: opencv python cam -- save feature

--- opencv/python/cam/main.py
+++ opencv/python/cam/main.py
@@ -1,15 +1,30 @@
 import cv2
+import time
+from datetime import datetime
+
 
 def main():
+    save = False
     cap = cv2.VideoCapture(0)
     while True:
         _, frame = cap.read()
         frame = cv2.flip(frame, 1)
         cv2.imshow('preview', frame)
 
+        if save:
+            t = datetime.now()
+            date = t.strftime("%Y%m%d_%H%M%S.%f")[:-3]
+            cv2.imwrite('preview/img-{}.jpg'.format(date), frame)
+
         key = cv2.waitKey(1)
         if key == ord('q'):
             break
+        if key == ord('s'):
+            save = ~save
+            if save:
+                print('START SAVE!')
+            else:
+                print('STOP SAVE!')
 
 if __name__ == '__main__':
     main()
