e121a0694fb1f1188bb019d0b7af6d9720135009
bjtj <bjtj10@gmail.com>
2019-01-03 14:28:40 +0900

    update: qt hello example -- print qt version

--- qt/hello/main.cpp
+++ qt/hello/main.cpp
@@ -1,8 +1,15 @@
+#include <QtCore>
 #include <QApplication>
 #include <QWidget>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
+
+    cout << "QT version: " << qVersion() << endl;
+    
     QApplication app(argc, argv);
     QWidget window;
     window.resize(250, 150);
