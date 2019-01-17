42085aac663d80a4870de0113d36800d478e5efb
bjtj <bjtj10@gmail.com>
2019-01-17 10:51:59 +0900

    update: python argparse -- nargs examples

--- python/argparse/main.py
+++ python/argparse/main.py
@@ -1,5 +1,59 @@
 import os
+import sys
 import argparse
+
+def nargs():
+    
+    # https://docs.python.org/3/library/argparse.html#nargs
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--foo', nargs=2)
+    parser.add_argument('bar', nargs=1)
+    print(parser.parse_args('c --foo a b'.split())) # Namespace(bar=['c'], foo=['a', 'b'])
+    print(parser.parse_args('--foo a b c'.split())) # Namespace(bar=['c'], foo=['a', 'b'])
+
+    assert(parser.parse_args('c --foo a b'.split()) == parser.parse_args('--foo a b c'.split()))
+
+    # ?
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--foo', nargs='?', const='c', default='d')
+    parser.add_argument('bar', nargs='?', default='d')
+    print(parser.parse_args(['XX', '--foo', 'YY'])) # Namespace(bar='XX', foo='YY')
+    print(parser.parse_args(['XX', '--foo']))       # Namespace(bar='XX', foo='c')
+    print(parser.parse_args([]))                    # Namespace(bar='d', foo='d')
+
+    # ? with optional input and output files
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+    print(parser.parse_args(['input.txt', 'output.txt']))
+
+    os.remove('output.txt')
+
+    print(parser.parse_args([]))
+
+    # *
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--foo', nargs='*')
+    parser.add_argument('--bar', nargs='*')
+    parser.add_argument('baz', nargs='*')
+    print(parser.parse_args('a b --foo x y --bar 1 2'.split()))
+
+
+    # +
+    parser = argparse.ArgumentParser(prog='PROG')
+    parser.add_argument('foo', nargs='+')
+    print(parser.parse_args(['a', 'b']))
+    try:
+        print(parser.parse_args([]))
+    except:
+        print('error')
+
+    # argparse.REMAINDER
+    parser = argparse.ArgumentParser(prog='PROG')
+    parser.add_argument('--foo')
+    parser.add_argument('command')
+    parser.add_argument('args', nargs=argparse.REMAINDER)
+    print(parser.parse_args('--foo B cmd --arg1 XX ZZ'.split())) # Namespace(args=['--arg1', 'XX', 'ZZ'], command='cmd', foo='B')
 
 
 def main():
@@ -14,6 +68,9 @@ def main():
     print(args)
     args, unknown = parser.parse_known_args()
     print('{} {}'.format(args, unknown))
+
+
+    nargs()
 
 if __name__ == '__main__':
     main()
