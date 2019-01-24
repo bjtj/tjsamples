36e729c6c10469da396385df6dfe3b089b0a5ff7
bjtj <bjtj10@gmail.com>
2019-01-24 13:13:16 +0900

    added: win32 visual studio -- dependency project example

---
+++ win32/dependency/libhello/libhello/hello.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	void __declspec(dllexport) hello(void);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ win32/dependency/libhello/libhello/stdafx.h
@@ -0,0 +1,16 @@
+﻿// stdafx.h : 자주 사용하지만 자주 변경되지는 않는
+// 표준 시스템 포함 파일 또는 프로젝트 특정 포함 파일이 들어 있는
+// 포함 파일입니다.
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+// Windows 헤더 파일
+#include <windows.h>
+
+
+
+// 여기서 프로그램에 필요한 추가 헤더를 참조합니다.
