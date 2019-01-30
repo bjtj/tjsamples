b845cfe0d53ce9c64c8b3d0616883688f9176384
bjtj <bjtj10@gmail.com>
2019-01-30 15:38:21 +0900

    update: java io -- console password masking

--- java/io/src/Console.java
+++ java/io/src/Console.java
@@ -6,5 +6,17 @@ class Console {
 	System.out.print("Enter: ");
 	String line = reader.readLine();
 	System.out.println("Your input - " + line);
+
+	// password masking
+	// - https://stackoverflow.com/a/8138549
+
+	java.io.Console console = System.console();
+	if (console == null) {
+	    System.out.println("Couldn't get Console instance");
+	    System.exit(0);
+	}
+
+	char[] passwordArray = console.readPassword("Enter your password: ");
+	console.printf("Password entered was %s%n", new String(passwordArray));
     }
 }
