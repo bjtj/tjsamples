cdf5f75855dfab3abe835e734610058bda929d49
bjtj <bjtj10@gmail.com>
2019-02-14 10:33:55 +0900

    update: python configparser for python3

--- python/config/main.py
+++ python/config/main.py
@@ -1,16 +1,25 @@
-import ConfigParser
+try:
+    import ConfigParser as configparser
+except Exception:
+    import configparser
 
 def main():
-    parser = ConfigParser.ConfigParser()
+    parser = configparser.ConfigParser()
     print(parser.read('conf.ini'))
     print(parser.get('default', 'name'))
     print(parser.get('default', 'job'))
     try:
         print(parser.get('default', 'noname'))
-    except Exception:
-        pass
+    except Exception as e:
+        print('error: {}'.format(e))
+        
     print(parser.get('default', 'msg'))
-    print(parser.get('default', 'msg', 1))
+    
+    try:
+        print(parser.get('default', 'msg', 1))
+    except Exception as e:
+        print('error: {}'.format(e))
+    
     
 
 if __name__ == '__main__':
