472dab834d43b8b31edfbc4826dd911eec93a0bb
bjtj <bjtj10@gmail.com>
2019-02-15 13:08:55 +0900

    fixed: ruby install script

--- ruby/install/ubuntu16.04.sh
+++ ruby/install/ubuntu16.04.sh
@@ -6,3 +6,17 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 
 sudo apt update
 sudo apt install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+
+cd
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+
+. ~/.bashrc
+
+rbenv install 2.6.1
+rbenv global 2.6.1
+ruby -v
