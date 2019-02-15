a1678d864f5f3ff9a8422c53a2b4913433db831a
bjtj <bjtj10@gmail.com>
2019-02-15 12:47:20 +0900

    added: ruby install script

---
+++ ruby/install/README.md
@@ -0,0 +1,3 @@
+# Installing ruby on Ubuntu 16.04
+
+* https://gorails.com/setup/ubuntu/16.04
---
+++ ruby/install/ubuntu16.04.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn~
