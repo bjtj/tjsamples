19de692fbe7d064c769485afe5fc31f60ccb3f11
bjtj <bjtj10@gmail.com>
2019-03-25 12:37:12 +0900

    added: python format example

---
+++ python/string/format.py
@@ -0,0 +1,11 @@
+
+def main():
+    print('Hello, {}.'.format('Steve'))
+    print('Float number: {:.3f}'.format(0.123456))
+    print('{{x: {}}}'.format('1'))
+    print('item1: {}, \
+item2: {}, \
+item3: {}'.format(1,2,3))
+
+if __name__ == '__main__':
+    main()
