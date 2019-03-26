e4ad1f46cb74a97a0794c8a3c9f1e6daae2c8c3e
bjtj <bjtj10@gmail.com>
2019-03-26 10:39:58 +0900

    update: shell -- get last dir of a path

--- shell/filesystem/pathname.sh
+++ shell/filesystem/pathname.sh
@@ -2,6 +2,15 @@
 
 # https://stackoverflow.com/a/965072/5676460
 
+function last {
+    x=""
+    while [ -n "$1" ]; do
+	x="$1"
+	shift
+    done
+    echo "$x"
+}
+
 filename=$(basename -- "a/b/c/file.mp3")
 extension="${filename##*.}"
 name="${filename%.*}"
@@ -10,3 +19,15 @@ echo "$filename"
 echo "$extension"
 echo "$name"
 echo "${filename##*/}"
+
+echo "-*- dirname -*-"
+echo $(dirname "/a/b/c/file")
+echo $(dirname -z "/a/b/c/file")
+echo $(dirname "/a/b/c/file" "/e/f/g")
+echo $(dirname -z "/a/b/c/file" "/e/f/g")
+
+echo "-*- last dirname -*-"
+filepath="/a/b/c/file"
+TOKENS=$(sed s:/:' ':g <<< $(dirname $filepath))
+RET=$(last $TOKENS)
+echo "last -- '$RET' ($filepath)"
