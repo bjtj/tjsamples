7156db8c225d68d738375d5d3bcc8a3130747467
bjtj <bjtj10@gmail.com>
2019-04-04 10:44:53 +0900

    update: python -- parse string date

--- python/datetime/main.py
+++ python/datetime/main.py
@@ -1,9 +1,26 @@
 from datetime import datetime
 import time
+from dateutil.parser import parse
 
+print('== strftime ==')
 t = datetime.now()
 print('datetime.now(): %s' % t)
 print('filename format: %s' % t.strftime("%Y%m%d_%H%M%S.%f"))
 print('filename format: %s' % time.strftime("%Y%m%d_%H%M%S"))
 print('filename format: %s' % t.strftime("%Y%m%d_%H%M%S.%f")[:-3])
 
+print('== datetime.datetime.strptime() ==')
+print(datetime.strptime('2014-01-01 23:59:27', '%Y-%m-%d %H:%M:%S'))
+print(datetime.strptime('2018.11.01', '%Y.%m.%d'))
+print(type(datetime.strptime('2018.11.01', '%Y.%m.%d')))
+
+print('== dateutil.parser.parse() ==')
+print(parse('2014-01-01 23:59:27'))
+print(parse('2018.11.01'))
+print(type(parse('2018.11.01')))
+try:
+    print(parse('text'))
+except Exception as e:
+    print('expected: {}'.format(e))
+
+print('Done.')
