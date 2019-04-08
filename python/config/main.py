f0f60bdd1f635214555a6e8004720daa32cf33f4
bjtj <bjtj10@gmail.com>
2019-04-08 10:28:00 +0900

    fixed: python configparser -- python2

--- python/config/main.py
+++ python/config/main.py
@@ -8,14 +8,20 @@ import os
 
 def set_config(filename):
     parser = configparser.ConfigParser()
-    parser['myconfig'] = {'name1': 'value1',
-                          'name2': 'value2',}
+    try:
+        parser['myconfig'] = {'name1': 'value1',
+                              'name2': 'value2',}
+    except Exception:
+        parser.add_section('myconfig')
+        parser.set('myconfig', 'name1', 'value1')
+        parser.set('myconfig', 'name2', 'value2')
     with open(filename, 'w') as fout:
         parser.write(fout)
 
 
 def main():
     parser = configparser.ConfigParser()
+    print(parser.read('no-exists'))
     print(parser.read('conf.ini'))
     print(parser.get('default', 'name'))
     print(parser.get('default', 'job'))
