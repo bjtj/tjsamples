41e018ccecc17342d27b2979493ac0dba75058f5
bjtj <bjtj10@gmail.com>
2019-04-16 13:41:29 +0900

    added: python -- tkinter example

---
+++ python/tkinter/hello/README.md
@@ -0,0 +1,8 @@
+# Hello, Tkinter #
+
+* http://effbot.org/tkinterbook/tkinter-hello-tkinter.htm
+
+
+# tkinter — Python interface to Tcl/Tk #
+
+* https://docs.python.org/3/library/tkinter.html
---
+++ python/tkinter/hello/hello.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    w = tk.Label(root, text="Hello, World!")
+    w.pack()
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
