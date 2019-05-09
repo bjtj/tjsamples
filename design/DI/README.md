68ea1bc6453740ec6916a2d8ba4c1d0c2a2aeb57
bjtj <bjtj10@gmail.com>
2019-05-09 15:23:03 +0900

    minor: folder restructered

---
+++ design/DI/README.md
@@ -0,0 +1,3 @@
+# A quick intro to Dependency Injection: what it is, and when to use it #
+
+* https://medium.freecodecamp.org/a-quick-intro-to-dependency-injection-what-it-is-and-when-to-use-it-7578c84fa88f
--- python/mapfilerreduce/main.py
+++ python/mapfilerreduce/main.py
@@ -2,9 +2,9 @@ import os
 
 def main():
     items = [1,2,3,4,5]
-    print(map(lambda x: x**2, items))
-    print(filter(lambda x: x % 2 == 0, items))
-    print(reduce(lambda x, y: x + y, items))
+    print(map(lambda x: x**2, items)) # [1, 4, 9, 16, 25]
+    print(filter(lambda x: x % 2 == 0, items)) # [2, 4]
+    print(reduce(lambda x, y: x + y, items))   # 15
 
 if __name__ == '__main__':
     main()
