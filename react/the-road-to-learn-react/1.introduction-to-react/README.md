3d6ef9c5be97295b57befed3e48a521823492d9c
bjtj <bjtj10@gmail.com>
2019-05-10 13:24:13 +0900

    added: react -- the road to learn react example

---
+++ react/the-road-to-learn-react/1.introduction-to-react/README.md
@@ -0,0 +1,9 @@
+```
+npm install -g create-react-app
+create-react-app --version
+create-react-app hackernews
+cd hackernews
+npm start
+npm test
+npm run build
+```
---
+++ react/the-road-to-learn-react/1.introduction-to-react/hackernews/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const list = [
+    {
+	title: 'React',
+	url: 'https://reactjs.org/',
+	author: 'Jordan Walke',
+	num_comments: 3,
+	points: 4,
+	objectID: 0,
+    },
+    {
+	title: 'Redux',
+	url: 'https://redux.js.org/',
+	author: 'Dan Abramov, Andrew Clark',
+	num_comments: 2,
+	points: 5,
+	objectID: 1,
+    },
+];
+
+function App() {
+    return (
+	<div className="App">
+	  {
+	      list.map(item => {
+		  return (
+		      <div key={item.objectID}>
+			<span>
+			  <a href={item.url}>{item.title}</a>
+			</span>
+			<span>{item.author}</span>
+			<span>{item.num_comments}</span>
+			<span>{item.points}</span>
+		      </div>
+		  );
+	      })
+	  }
+	</div>
+    );
+}
+
+export default App;
