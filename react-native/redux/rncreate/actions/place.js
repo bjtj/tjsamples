f055017473d6fb71ea6a945b8033e1a9ca73b003
bjtj <bjtj10@gmail.com>
2019-06-05 13:33:24 +0900

    update: react native -- redux example - clear button added

--- react-native/redux/rncreate/actions/place.js
+++ react-native/redux/rncreate/actions/place.js
@@ -1,8 +1,16 @@
-import { ADD_PLACE } from './types';
+import { ADD_PLACE, CLEAR_PLACES } from './types';
 
 export const addPlace = placeName => {
-	return {
-		type: ADD_PLACE,
-		payload: placeName
-	};
+    return {
+	type: ADD_PLACE,
+	payload: placeName,
+    };
+};
+
+
+export const clearPlaces = () => {
+    return {
+	type: CLEAR_PLACES,
+	payload: null,
+    };
 };
