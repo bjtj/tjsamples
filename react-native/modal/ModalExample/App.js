12f8e37a6c0883b8757cfb2ce863ed12e4b1d164
bjtj <bjtj10@gmail.com>
2019-06-10 07:11:00 +0900

    added: react native -- modal example

---
+++ react-native/modal/ModalExample/App.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Button
+} from 'react-native';
+import Modal from "react-native-modal";
+
+
+type Props = {};
+export default class App extends Component<Props> {
+
+    constructor(props) {
+	super(props);
+	this.state = {
+	    isModalVisible: false,
+	};
+    }
+
+    _toggleModal() {
+	this.setState({ isModalVisible: !this.state.isModalVisible });
+    }
+    
+    render() {
+	return (
+	    <View style={styles.container}>
+	      <Button
+		title='Click'
+		onPress={() => this._toggleModal()}>
+	      </Button>
+	      <Modal isVisible={this.state.isModalVisible}>
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+		  <Text
+		    style={{ color: 'white', fontSize: 24, margin: 10 }}>
+		    Hello!
+		  </Text>
+		  <Button
+		    title='Close'
+		    onPress={() => this._toggleModal()}>
+		  </Button>
+		</View>
+	      </Modal>
+	    </View>
+	);
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+	flex: 1,
+	justifyContent: 'center',
+	alignItems: 'center',
+	backgroundColor: '#F5FCFF',
+    },
+
+});
---
+++ react-native/modal/ModalExample/README.md
@@ -0,0 +1,7 @@
+# react-native-modal #
+
+* https://github.com/react-native-community/react-native-modal
+
+```
+yarn add react-native-modal
+```
