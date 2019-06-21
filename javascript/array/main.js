8c9577c546a92dbe70b65052fd577ecc18c65c9c
bjtj <bjtj10@gmail.com>
2019-06-21 18:17:31 +0900

    added: javascript -- array example

---
+++ javascript/array/main.js
@@ -0,0 +1,10 @@
+
+
+
+console.log([1,2,3]);
+console.log([1,2,3].slice(0, 1));
+console.log([1,2,3].slice(0, 2));
+console.log([1,2,3].slice(1, 1));
+console.log([1,2,3].slice(1, 2));
+console.log([1,2,3].slice(1, 6));
+console.log([1,2,3].slice(1));
