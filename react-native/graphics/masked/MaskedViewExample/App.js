010b8f2ca69812bcdc27afce4146197ef247b54e
bjtj <bjtj10@gmail.com>
2019-06-23 13:06:51 +0900

    added: react native -- masked view example

---
+++ react-native/graphics/masked/MaskedViewExample/App.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import MaskedView from '@react-native-community/masked-view';
+
+
+type Props = {};
+export default class App extends Component<Props> {
+    render() {
+	return (
+	    <MaskedView
+              style={{flex: 1, flexDirection: 'row', height: '100%'}}
+              maskElement={
+		      <View
+			    style={{
+				// Transparent background because mask is based off alpha channel.
+				backgroundColor: 'transparent',
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+			    }}>
+			    <Text
+				  style={{
+				      fontSize: 60,
+				      color: 'black',
+				      fontWeight: 'bold',
+				  }}>
+				  Basic Mask
+				</Text>
+			  </View>
+		      }>
+              {/* Shows behind the mask, you can put anything here, such as an image */}
+              <View style={{flex: 1, height: '100%', backgroundColor: '#324376'}} />
+              <View style={{flex: 1, height: '100%', backgroundColor: '#F5DD90'}} />
+              <View style={{flex: 1, height: '100%', backgroundColor: '#F76C5E'}} />
+              <View style={{flex: 1, height: '100%', backgroundColor: '#e1e1e1'}} />
+	    </MaskedView>
+	);
+    }
+}
+
+const styles = StyleSheet.create({
+
+});
---
+++ react-native/graphics/masked/MaskedViewExample/README.md
@@ -0,0 +1,7 @@
+* https://github.com/react-native-community/react-native-masked-view
+
+
+```
+$ yarn add @react-native-community/masked-view
+$ react-native link @react-native-community/masked-view
+```
