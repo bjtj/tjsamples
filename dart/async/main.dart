7b660b356782540432bd7bb8991e9a8f6025d7c8
bjtj <bjtj10@gmail.com>
2019-07-11 09:59:21 +0900

    added: dart -- async

---
+++ dart/async/main.dart
@@ -0,0 +1,47 @@
+// https://dart.dev/tutorials/language/futures
+
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+
+Future<void> printDailyNewsDigest() async {
+  var newsDigest = await gatherNewsReports();
+  print(newsDigest);
+}
+
+main(List<String> args) {
+  printDailyNewsDigest();
+  printWinningLotteryNumbers();
+  printWeatherForecast();
+  printBaseballScore();
+}
+
+printWinningLotteryNumbers() {
+  print('Winning lotto numbers: [23, 64, 87, 26, 2]');
+}
+
+printWeatherForecast() {
+  print("Tomorrow's forecast: 70F, sunny.");
+}
+
+printBaseballScore() {
+  print('Baseball score: Red Sox 10, Yankees 0');
+}
+
+const news = '<gathered news goes here>';
+const oneSecond = Duration(seconds: 1);
+
+// Imagine that this function is more complex and slow. :)
+Future<String> gatherNewsReports() =>
+Future.delayed(oneSecond, () => news);
+
+// Alternatively, you can get news from a server using features
+// from either dart:io or dart:html. For example:
+//
+// import 'dart:html';
+//
+// Future<String> gatherNewsReportsFromServer() => HttpRequest.getString(
+//      'https://www.dartlang.org/f/dailyNewsDigest.txt',
+//    );
