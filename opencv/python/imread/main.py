297c63ddad82bf615023eeaac6140c43274001a6
bjtj <bjtj10@gmail.com>
2019-09-09 13:00:37 +0900

    update: opencv -- width, height, channels

--- opencv/python/imread/main.py
+++ opencv/python/imread/main.py
@@ -3,6 +3,8 @@ import cv2
 def main():
     # https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_image_display/py_image_display.html
     img = cv2.imread('Lenna.png', cv2.IMREAD_COLOR)
+    h, w, c = img.shape
+    print(f'width: {w}, height: {h}, channel: {c}')
     cv2.imshow('lena', img)
     cv2.waitKey(0)
 
