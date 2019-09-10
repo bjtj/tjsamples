79b1d74200435fb0e8985055e78b9c18617fbdf0
bjtj <bjtj10@gmail.com>
2019-09-10 10:05:55 +0900

    added: shell -- bash colon

---
+++ shell/bash/README.md
@@ -0,0 +1,6 @@
+
+# colon #
+
+https://stackoverflow.com/a/32343069/5676460
+
+https://stackoverflow.com/a/3224910/5676460
---
+++ shell/bash/colon.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+:
+: ${POSTGRES_USER:=postgres}
+# ${POSTGRES_USER:=postgres}
+${POSTGRES_USER:=postgres}
