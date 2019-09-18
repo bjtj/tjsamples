f536f302e600cd6053363baf9574428b9cca9b0b
bjtj <bjtj10@gmail.com>
2019-09-18 10:08:14 +0900

    minor: fixed -- emacs install script typo

--- emacs/install/emacs26.1.sh
+++ emacs/install/emacs26.1.sh
@@ -3,7 +3,7 @@
 set -e
 
 mkdir -p /tmp/emacs
-cd /tmp/eamcs
+cd /tmp/emacs
 sudo apt install libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgnutls-dev libncurses-dev
 curl -OL http://ftp.jaist.ac.jp/pub/GNU/emacs/emacs-26.1.tar.gz
 tar xvf emacs-26.1.tar.gz
