cb956c80be1442663f2ad2460f36854676e82b0a
bjtj <bjtj10@gmail.com>
2019-09-19 14:15:39 +0900

    minor: emacs script '-y' is added to apt install

--- emacs/install/emacs26.1.sh
+++ emacs/install/emacs26.1.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir -p /tmp/emacs
 cd /tmp/emacs
-sudo apt install libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgnutls-dev libncurses-dev
+sudo apt install -y libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgnutls-dev libncurses-dev
 curl -OL http://ftp.jaist.ac.jp/pub/GNU/emacs/emacs-26.1.tar.gz
 tar xvf emacs-26.1.tar.gz
 cd emacs-26.1
