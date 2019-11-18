b1c9dc471a5741c28dbb41571a8453c0f092e08b
bjtj <bjtj10@gmail.com>
2019-11-18 16:39:18 +0900

    update: python flask -- send_file

--- python/flask/app/file.py
+++ python/flask/app/file.py
@@ -1,6 +1,12 @@
-from flask import Blueprint, request, Response, render_template
+import os
+from flask import Blueprint, request, Response, render_template, send_file, redirect
 
 file = Blueprint('file', __name__, url_prefix='/file', template_folder='templates')
+
+BASE_PATH='downloads'
+if not os.path.exists(BASE_PATH):
+    os.makedirs(BASE_PATH)
+
 
 @file.route('/')
 def index():
@@ -12,10 +18,34 @@ def upload():
     save = request.form.get('save')
     for k, v in request.files.items():
         print('Name: {}'.format(v.name))
+        print('File name: {}'.format(v.filename))
         print('Content type: {}'.format(v.headers['Content-Type']))
         print('Content type: {}'.format(v.content_type))
         if save:
             print('== Write to file ==')
-            open(v.name, 'wb').write(v.read())
-    return Response('\n'.join(request.files.keys()),
-                    mimetype='text/plain')
+            open(os.path.join(BASE_PATH, v.filename), 'wb').write(v.read())
+    res = '\n'.join([x.filename for x in request.files.values()])
+    if save:
+        return redirect('list')
+    else:
+        return Response(res, mimetype='text/plain')
+
+
+class FileItem:
+    def __init__(self, basepath, filename):
+        self.basepath = basepath
+        self.filename = filename
+        self.path = os.path.join(self.basepath, self.filename)
+
+
+@file.route('/list')
+def list():
+    items = [FileItem(BASE_PATH, x) for x in os.listdir(BASE_PATH)]
+    return render_template('file/list.html', files = items)
+
+
+@file.route('/download/<filename>')
+def download(filename):
+    filepath = os.path.join(BASE_PATH, filename)
+    return send_file(filepath)
+    
