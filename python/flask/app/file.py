2e72ed594cfe56db0a6f90035fbbb744a51dea69
bjtj <bjtj10@gmail.com>
2019-11-19 09:12:59 +0900

    update: python flask -- upload file size

--- python/flask/app/file.py
+++ python/flask/app/file.py
@@ -17,13 +17,21 @@ def index():
 def upload():
     save = request.form.get('save')
     for k, v in request.files.items():
+        print('Type: {}'.format(type(v)))
         print('Name: {}'.format(v.name))
         print('File name: {}'.format(v.filename))
         print('Content type: {}'.format(v.headers['Content-Type']))
         print('Content type: {}'.format(v.content_type))
+        print('Content length: {}'.format(v.content_length)) # 0
         if save:
             print('== Write to file ==')
-            open(os.path.join(BASE_PATH, v.filename), 'wb').write(v.read())
+            # open(os.path.join(BASE_PATH, v.filename), 'wb').write(v.read())
+            filepath = os.path.join(BASE_PATH, v.filename)
+            v.save(filepath)    # https://stackoverflow.com/a/15782516/5676460
+            print('File size: {}'.format(os.stat(filepath).st_size))
+        else:
+            print('File size: {}'.format(len(v.read())))
+            
     res = '\n'.join([x.filename for x in request.files.values()])
     if save:
         return redirect('list')
