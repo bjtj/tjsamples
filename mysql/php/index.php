274b93a4b6a4cab2a679aecba4c0e26023a9471f
bjtj <bjtj10@gmail.com>
2019-12-17 11:05:20 +0900

    update: mysql php

--- mysql/php/index.php
+++ mysql/php/index.php
@@ -1,6 +1,11 @@
+<?php header('Content-Type: text/plain'); ?>
 <?php
 
 $ini = parse_ini_file("config.ini");
+if (!$ini) {
+    echo "[ERROR] No config.ini File";
+    exit(1);
+}
 $host = $ini["host"];
 $port = $ini["port"];
 $username = $ini["username"];
@@ -10,18 +15,23 @@ $database = $ini["database"];
 
 $conn = mysqli_connect($host, $username, $passwd, $database, $port);
 
+echo "[X] Drop Table\n";
 $sql = "DROP TABLE IF EXISTS `test`;";
 $ret = mysqli_query($conn, $sql);
-echo "Drop table: '" . $ret . "'\n";
+echo "Result: '" . $ret . "'\n";
 
+echo "[X] Create Table\n";
 $sql = "CREATE TABLE IF NOT EXISTS `test` (`id` int(11) AUTO_INCREMENT, `body` text, PRIMARY KEY(`id`));";
 $ret = mysqli_query($conn, $sql);
-echo "Create table: '" . $ret . "'\n";
+echo "Result: '" . $ret . "'\n";
 
-$sql = "INSERT INTO `test` (body) VALUES ('Hello');";
+echo "[X] Insert\n";
+$hello = "Hello";
+$sql = "INSERT INTO `test` (body) VALUES ('$hello');";
 $ret = mysqli_query($conn, $sql);
-echo "Insert: '" . $ret . "'\n";
+echo "Result: '" . $ret . "'\n";
 
+echo "[X] Select\n";
 $sql = "SELECT * FROM `test`;";
 $ret = mysqli_query($conn, $sql);
 if (mysqli_num_rows($ret) > 0) {
