5a9adf3215355624956fb633c102ede812ce3f74
bjtj <bjtj10@gmail.com>
2020-05-19 11:20:33 +0900

    added: pygobject - layout containers

--- gtk/python/README.md
+++ gtk/python/README.md
@@ -10,3 +10,33 @@
 # Threads & Concurrency #
 
 <https://pygobject.readthedocs.io/en/latest/guide/threading.html>
+
+
+# Fade out #
+
+<https://askubuntu.com/a/216903>
+
+```css
+GtkLabel:insensitive {
+    color: rgba(255,255,255,0);
+    transition: 500ms ease-in-out;
+}
+```
+
+e.g.)
+
+```python
+label.set_state_flags(Gtk.StateFlags.INSENSITIVE, True)
+```
+
+# 6. Layout Containers #
+
+<https://python-gtk-3-tutorial.readthedocs.io/en/latest/layout.html>
+
+* Boxes
+* Grid
+* ListBox
+* Stack and StackSwitcher
+* headerBar
+* FlowBox
+* Notebook
---
+++ gtk/python/layout/notebook.py
@@ -0,0 +1,31 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Simple Notebook Example")
+        self.set_border_width(3)
+
+        self.notebook = Gtk.Notebook()
+        self.add(self.notebook)
+
+        self.page1 = Gtk.Box()
+        self.page1.set_border_width(10)
+        self.page1.add(Gtk.Label("Default Page!"))
+        self.notebook.append_page(self.page1, Gtk.Label("Plain Title"))
+
+        self.page2 = Gtk.Box()
+        self.page2.set_border_width(10)
+        self.page2.add(Gtk.Label("A page with an image for a Title."))
+        self.notebook.append_page(
+            self.page2, Gtk.Image.new_from_icon_name("help-about", Gtk.IconSize.MENU)
+        )
+
+
+win = MyWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
