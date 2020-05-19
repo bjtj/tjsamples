92cd782c2466553d73b048c6016b748b5513fae5
bjtj <bjtj10@gmail.com>
2020-05-19 11:34:04 +0900

    added: pygobject - glade example

---
+++ gtk/python/glade/example.py
@@ -0,0 +1,22 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class Handler:
+    def onDestroy(self, *args):
+        Gtk.main_quit()
+
+    def onButtonPressed(self, button):
+        print("Hello World!")
+
+
+builder = Gtk.Builder()
+builder.add_from_file("builder_example.glade")
+builder.connect_signals(Handler())
+
+window = builder.get_object("window1")
+window.show_all()
+
+Gtk.main()
