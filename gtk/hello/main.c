3405e9255c85a5fc67ee20fec057e8cc274babf9
bjtj <bjtj10@gmail.com>
2020-05-23 14:40:39 +0900

    minor: gtk hello

--- gtk/hello/main.c
+++ gtk/hello/main.c
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     gtk_init(&argc, &argv);
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_widget_show(window);
+    g_signal_connect(GTK_WINDOW(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
     gtk_main();
     return 0;
 }
