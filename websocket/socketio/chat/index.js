b5000ac11b46266bac5fece74b6dfe11db98ebb0
bjtj <bjtj10@gmail.com>
2020-07-18 14:32:35 +0900

    added: socket.io chat example

---
+++ websocket/socketio/chat/index.js
@@ -0,0 +1,22 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  io.emit("chat message", "a user is connected");
+  socket.on("disconnect", () => {
+    io.emit("chat message", "a user is disconnected");
+  });
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
