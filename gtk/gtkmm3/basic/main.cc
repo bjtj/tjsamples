a341e66035926fc2393ddfdb855647375ca27761
bjtj <bjtj10@gmail.com>
2020-08-06 13:56:44 +0900

    added: gtk -- gtkmm3 example

---
+++ gtk/gtkmm3/basic/main.cc
@@ -0,0 +1,13 @@
+#include <gtkmm.h>
+
+int main(int argc, char *argv[])
+{
+    auto app =
+	Gtk::Application::create(argc, argv,
+				 "org.gtkmm.examples.base");
+
+    Gtk::Window window;
+    window.set_default_size(200, 200);
+
+    return app->run(window);
+}
