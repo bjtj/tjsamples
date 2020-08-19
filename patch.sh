b42616044832944c80344fd4eb69791d435d95cd
bjtj <bjtj10@gmail.com>
2020-08-19 09:02:43 +0900

    fixed: vulnerability - dot-pop@^4.2.1

--- patch.sh
+++ patch.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 
-DIRS=("react/material/basic/"
+# DIRS=("react/material/basic/"
+#       "react/my-app/"
+#       "react/the-road-to-learn-react/2.basics-in-react/hackernews/"
+#       "react/calc/"
+#       "react-native/first-project/"
+#      )
+
+# CMD="yarn upgrade websocket-extensions@^0.1.4"
+
+DIRS=("react/the-road-to-learn-react/6.state-management-in-react/hackernews/"
+      "react/the-road-to-learn-react/4.code-organization-and-testing/hackernews/"
+      "react/the-road-to-learn-react/3.getting-real-with-apis/hackernews/"
+      "react/redux/todo/"
+      "react/react-express/client/"
+      "react/material/basic/"
+      "nodejs/express/rest/"
       "react/my-app/"
       "react/the-road-to-learn-react/2.basics-in-react/hackernews/"
       "react/calc/"
       "react-native/first-project/"
      )
 
-CMD="yarn upgrade websocket-extensions@^0.1.4"
+CMD="yarn upgrade dot-prop@^4.2.1"
 
 for i in ${DIRS[@]}; do
     echo $i
