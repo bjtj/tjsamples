b3faee64ba1fd112ee0fbd909d601ad1f66ad492
bjtj <bjtj10@gmail.com>
2020-08-21 10:06:56 +0900

    updated: python logging format

--- python/logging/basic.py
+++ python/logging/basic.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+
+"""basic
+"""
+
 import logging
 
 
@@ -8,4 +13,18 @@ logging.basicConfig(level=logging.DEBUG,
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-logger.debug('hello')
+
+def main():
+    """main
+    """
+    logger.debug('hello')
+    name = 'TJ'
+    logger.debug('hello %s', name)
+    logger.debug('%.2f', 1.23456789)
+    logger.debug('%d', 123456789)
+    logger.debug('%05d', 12)
+    logger.debug('%05x', 12)
+    logger.debug('%05X', 0xff)
+
+if __name__ == '__main__':
+    main()
