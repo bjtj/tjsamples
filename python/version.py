95c2ead15c61a6bb7beed43dc0ba3f19d44d8771
bjtj <bjtj10@gmail.com>
2020-09-06 07:37:23 +0900

    added: python - namedtuple example

--- python/version.py
+++ python/version.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python
+
 # https://stackoverflow.com/a/9079062
-import os
 import sys
 
 
 def main():
+    """main
+    """
     print(sys.version_info[0])
+    print('major:', sys.version_info.major)
+    print('minor:', sys.version_info.minor)
     print(sys.version_info)
+
 
 if __name__ == '__main__':
     main()
