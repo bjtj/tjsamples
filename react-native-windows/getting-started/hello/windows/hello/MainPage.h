eb8d00c285c6e66f03cac91f0bd0de104a295512
bjtj <bjtj10@gmail.com>
2020-09-12 15:47:18 +0900

    added: react native windows example

---
+++ react-native-windows/getting-started/hello/windows/hello/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+
+namespace winrt::hello::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::hello::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
+
