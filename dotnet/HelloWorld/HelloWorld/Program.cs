62e9bcf525c305625c42b898165a7f4465cfe656
bjtj <bjtj10@gmail.com>
2020-12-15 17:48:31 +0900

    added: dotnet visual studio example

---
+++ dotnet/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
