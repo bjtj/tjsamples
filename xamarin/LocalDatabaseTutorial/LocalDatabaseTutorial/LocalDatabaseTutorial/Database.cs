59d55c7f445ec063f59a09eea0f925fd47852dd5
bjtj <bjtj10@gmail.com>
2020-12-20 22:55:17 +0900

    Added: Xamarin - Xamarin.Forms Local Database Tutorial

---
+++ xamarin/LocalDatabaseTutorial/LocalDatabaseTutorial/LocalDatabaseTutorial/Database.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace LocalDatabaseTutorial
+{
+    public class Database
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Person>().Wait();
+        }
+
+        public Task<List<Person>> GetPeopleAsync()
+        {
+            return _database.Table<Person>().ToListAsync();
+        }
+
+        public Task<int> SavePersonAsync(Person person)
+        {
+            return _database.InsertAsync(person);
+        }
+    }
+}
---
+++ xamarin/LocalDatabaseTutorial/README.md
@@ -0,0 +1,6 @@
+# Xamarin.Forms Local Database Tutorial #
+
+<https://docs.microsoft.com/en-us/xamarin/get-started/tutorials/local-database/?tabs=vswin>
+
+nuget package
+* sqlite-net-pcl
