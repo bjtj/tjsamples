ebd9dde7a6512c0b85cd25070beba44faaa1fcc3
bjtj <bjtj10@gmail.com>
2020-12-20 12:17:58 +0900

    Added: wpf tutorial

---
+++ wpf/ExpenseIt/ExpenseIt/ExpenseItHome.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpenseIt
+{
+    /// <summary>
+    /// ExpenseItHome.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ExpenseItHome : Page
+    {
+        public ExpenseItHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+        }
+    }
+}
---
+++ wpf/ExpenseIt/README.md
@@ -0,0 +1,4 @@
+# Tutorial: Create your first WPF application in Visual Studio 2019 #
+
+<https://docs.microsoft.com/en-us/dotnet/desktop/wpf/getting-started/walkthrough-my-first-wpf-desktop-application?view=netframeworkdesktop-4.8>
+
