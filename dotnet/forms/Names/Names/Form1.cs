39366c75c2d672f4c8fe79640119060eebebcbac
bjtj <bjtj10@gmail.com>
2020-12-20 15:26:15 +0900

    Added: windows forms tutorial

---
+++ dotnet/forms/Names/Names/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Names
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+                lstNames.Items.Add(txtName.Text);
+        }
+    }
+}
---
+++ dotnet/forms/README.md
@@ -0,0 +1,4 @@
+# Windows Forms documentation #
+
+<https://docs.microsoft.com/en-us/dotnet/desktop/winforms/?view=netdesktop-5.0&preserve-view=true>
+
