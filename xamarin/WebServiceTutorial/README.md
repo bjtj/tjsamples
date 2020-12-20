c7125f6beffad77f531e6468a0c3be5f3e283844
bjtj <bjtj10@gmail.com>
2020-12-20 23:14:26 +0900

    Added: Xamarin - Xamarin.Forms Web Service Tutorial

---
+++ xamarin/WebServiceTutorial/README.md
@@ -0,0 +1,14 @@
+# Xamarin.Forms Web Service Tutorial #
+
+<https://docs.microsoft.com/en-us/xamarin/get-started/tutorials/web-service/?tabs=vswin>
+
+nuget package
+* Newtonsoft.Json
+
+## OpenWeather ##
+
+<https://openweathermap.org/>
+
+API key
+
+> You must set your personal OpenWeatherMap API key as the value of the `OpenWeatherMapAPIKey` constant.
---
+++ xamarin/WebServiceTutorial/WebServiceTutorial/WebServiceTutorial/WeatherData.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+
+namespace WebServiceTutorial
+{
+    public class WeatherData
+    {
+        [JsonProperty("name")]
+        public string Title { get; set; }
+
+        [JsonProperty("weather")]
+        public Weather[] Weather { get; set; }
+
+        [JsonProperty("main")]
+        public Main Main { get; set; }
+
+        [JsonProperty("visibility")]
+        public long Visibility { get; set; }
+
+        [JsonProperty("wind")]
+        public Wind Wind { get; set; }
+    }
+
+    public class Main
+    {
+
+        double temperature;
+
+        [JsonProperty("temp")]
+        public double Temperature {
+            get { return temperature; }
+            set { temperature = value; }
+        }
+        
+        public double TemperatureCelsius {
+            get { 
+                return (temperature - 32) * 5 / 9;
+            }
+        }
+
+        [JsonProperty("humidity")]
+        public long Humidity { get; set; }
+    }
+
+    public class Weather
+    {
+        [JsonProperty("main")]
+        public string Visibility { get; set; }
+    }
+
+    public class Wind
+    {
+        [JsonProperty("speed")]
+        public double Speed { get; set; }
+    }
+}
