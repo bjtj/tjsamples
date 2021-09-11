0cadc60a9dd25f279f704ce24a77b0e334c70774
bjtj <bjtj10@gmail.com>
2021-09-11 23:25:02 +0900

    clojure luminus: refine your app -- managing database migrations, querying the database

--- clojure/luminus/web-development-with-clojure/guestbook/resources/sql/queries.sql
+++ clojure/luminus/web-development-with-clojure/guestbook/resources/sql/queries.sql
@@ -1,21 +1,8 @@
--- :name create-user! :! :n
--- :doc creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
-
--- :name update-user! :! :n
--- :doc updates an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
-WHERE id = :id
-
--- :name get-user :? :1
--- :doc retrieves a user record given the id
-SELECT * FROM users
-WHERE id = :id
-
--- :name delete-user! :! :n
--- :doc deletes a user record given the id
-DELETE FROM users
-WHERE id = :id
+-- :name save-message! :! :n
+-- :doc creates a new message using the name and message keys
+INSERT INTO guestbook
+(name, message)
+VALUES (:name, :message)
+-- :name get-messages :? :*
+-- :doc selects all available messages
+SELECT * FROM guestbook
