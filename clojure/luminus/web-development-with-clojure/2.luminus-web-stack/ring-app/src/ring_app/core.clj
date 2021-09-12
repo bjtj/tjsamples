6192aef8520037ebdde240e4ceffcfc43a0ed9ea
bjtj <bjtj10@gmail.com>
2021-09-12 19:31:06 +0900

    clojure luminus: extend ring

--- clojure/luminus/web-development-with-clojure/2.luminus-web-stack/ring-app/src/ring_app/core.clj
+++ clojure/luminus/web-development-with-clojure/2.luminus-web-stack/ring-app/src/ring_app/core.clj
@@ -1,15 +1,25 @@
 (ns ring-app.core
-  (:require [ring.adapter.jetty :as jetty]
-            [ring.util.response :as response]
-            [ring.middleware.reload :refer [wrap-reload]]))
+  (:require
+   [muuntaja.middleware :as muuntaja]
+   [ring.adapter.jetty :as jetty]
+   [ring.util.http-response :as response]
+   [ring.middleware.reload :refer [wrap-reload]]))
 
-(defn handler
+(defn html-handler
   ""
   [request-map]
-  (response/response
-   (str "<html>Hello, Your IP is: "
+  (response/ok
+   (str "<html> your IP is: "
         (:remote-addr request-map)
         "</body></html>")))
+
+(defn json-handler
+  ""
+  [request]
+  (response/ok
+   {:result (get-in request [:body-params :id])}))
+
+(def handler json-handler)
 
 (defn wrap-nocache
   ""
@@ -19,12 +29,19 @@
         handler
         (assoc-in [:headers "Pragma"] "no-cache"))))
 
+(defn wrap-formats
+  ""
+  [handler]
+  (-> handler
+      (muuntaja/wrap-format)))
+
 (defn -main
   ""
   []
   (jetty/run-jetty
    (-> #'handler
        wrap-nocache
+       wrap-formats
        wrap-reload)
    {:port 3000
     :join? false}))
