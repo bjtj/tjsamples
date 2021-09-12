af7d85c13ed96a076c079ef42c4fbdefcd16c568
bjtj <bjtj10@gmail.com>
2021-09-12 19:12:50 +0900

    clojure luminus: creating a web server -- adding functionality with middleware

--- clojure/luminus/web-development-with-clojure/2.luminus-web-stack/ring-app/src/ring_app/core.clj
+++ clojure/luminus/web-development-with-clojure/2.luminus-web-stack/ring-app/src/ring_app/core.clj
@@ -1,29 +1,30 @@
 (ns ring-app.core
   (:require [ring.adapter.jetty :as jetty]
-            [ring.util.response :as response]))
-
-;; (defn handler
-;;   ""
-;;   [request-map]
-;;   {:status 200
-;;    :headers {"Content-Type" "text/html"}
-;;    :body (str "<html> Your IP is: "
-;;               (:remote-addr request-map)
-;;               "</body></html>")
-;;    })
+            [ring.util.response :as response]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 (defn handler
   ""
   [request-map]
   (response/response
-   (str "<html> Your IP is: "
+   (str "<html>Hello, Your IP is: "
         (:remote-addr request-map)
         "</body></html>")))
+
+(defn wrap-nocache
+  ""
+  [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cache"))))
 
 (defn -main
   ""
   []
   (jetty/run-jetty
-   handler
+   (-> #'handler
+       wrap-nocache
+       wrap-reload)
    {:port 3000
     :join? false}))
