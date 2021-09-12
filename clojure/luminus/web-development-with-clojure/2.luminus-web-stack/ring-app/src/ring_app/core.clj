9fb45d492a375f6e9a2589c0ea582c38e1b182b9
bjtj <bjtj10@gmail.com>
2021-09-12 19:53:29 +0900

    clojure luminus: define the routes with reitit -- reitit defaults

--- clojure/luminus/web-development-with-clojure/2.luminus-web-stack/ring-app/src/ring_app/core.clj
+++ clojure/luminus/web-development-with-clojure/2.luminus-web-stack/ring-app/src/ring_app/core.clj
@@ -60,7 +60,18 @@
 
 (def handler
   (reitit/ring-handler
-   (reitit/router routes)))
+   (reitit/router routes)
+   (reitit/create-resource-handler {:path "/"})
+   (reitit/create-default-handler
+    {:not-found
+     (constantly (response/not-found "404 - Page not found"))
+     :method-not-allowed
+     (constantly (response/method-not-allowed "405 - Not allowed"))
+     :not-acceptable
+     (constantly (response/not-acceptable "406 - Not acceptable"))})))
+
+;; $ curl localhost:3000/foo
+;; 404 - Page not found
 
 (defn -main
   ""
