56b6ab28a31c39c72f26ae42f6d878084628633d
bjtj <bjtj10@gmail.com>
2021-09-12 19:47:00 +0900

    clojure luminus: define the routes with reitit

--- clojure/luminus/web-development-with-clojure/2.luminus-web-stack/ring-app/project.clj
+++ clojure/luminus/web-development-with-clojure/2.luminus-web-stack/ring-app/project.clj
@@ -5,6 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [metosin/muuntaja "0.6.7"]
+                 [metosin/reitit "0.5.11"]
                  [metosin/ring-http-response "0.9.1"]
                  [ring "1.8.2"]]
   :repl-options {:init-ns ring-app.core}
