6cb9de6c4062f7fdbb7528d373e32b82132d425b
bjtj <bjtj10@gmail.com>
2021-09-25 22:34:26 +0900

    updated: web development with clojure - 4. introducing clojurescript -- build the ui with reagent

--- clojure/luminus/web-development-with-clojure/4.introducing-clojurescript/guestbook/project.clj
+++ clojure/luminus/web-development-with-clojure/4.introducing-clojurescript/guestbook/project.clj
@@ -31,7 +31,8 @@
                  [ring/ring-core "1.8.2"]
                  [ring/ring-defaults "0.3.2"]
                  [selmer "1.12.31"]
-                 [org.clojure/clojurescript "1.10.764" :scope "provided"]]
+                 [org.clojure/clojurescript "1.10.764" :scope "provided"]
+                 [reagent "1.0.0"]]
 
   :min-lein-version "2.0.0"
   
