327309c6d8ff303c0d6cb6d23bc1f6fb43dc13a8
bjtj <bjtj10@gmail.com>
2021-09-26 00:19:31 +0900

    updated: web development with clojure - 4. introducing clojurescript -- talking to the server

--- clojure/luminus/web-development-with-clojure/4.introducing-clojurescript/guestbook/src/cljs/guestbook/core.cljs
+++ clojure/luminus/web-development-with-clojure/4.introducing-clojurescript/guestbook/src/cljs/guestbook/core.cljs
@@ -1,6 +1,8 @@
 (ns guestbook.core
   (:require [reagent.core :as r]
-            [reagent.dom :as dom]))
+            [reagent.dom :as dom]
+            [ajax.core :refer [GET POST]]
+            [clojure.string :as string]))
 
 (dom/render
  [:h1 "Hello, Reagent"]
@@ -14,17 +16,41 @@
  [:div#hello.content>h1 "Hello, Auto!!"]
  (.getElementById js/document "content"))
 
+(defn send-message!
+  ""
+  [fields errors]
+  (POST "/message"
+        {:format :json
+         :headers
+         {"Accept" "application/transit+json"
+          "x-csrf-token" (.-value (.getElementById js/document "token"))}
+         :params @fields
+         :handler (fn [r]
+                    (.log js/console (str "response:" r))
+                    (reset! errors nil))
+         :error-handler (fn [e]
+                          (.log js/console (str e))
+                          (reset! errors (-> e :response :errors)))}))
+
+(defn errors-component
+  ""
+  [errors id]
+  (when-let [error (id @errors)]
+    [:div.notification.is-danger (string/join error)]))
 
 (defn message-form
   ""
   []
-  (let [fields (r/atom {})]
+  (let [fields (r/atom {})
+        errors (r/atom nil)]
     (fn []
       [:div
        [:p "Name: " (:name @fields)]
        [:p "Message: " (:message @fields)]
+       [errors-component errors :server-error]
        [:div.field
         [:label.label {:for :name} "Name"]
+        [errors-component errors :name]
         [:input.input
          {:type :text
           :name :name
@@ -33,6 +59,7 @@
           :value (:name @fields)}]]
        [:div.field
         [:label.label {:for :message} "Message"]
+        [errors-component errors :message]
         [:textarea.textarea
          {:name :message
           :value (:message @fields)
@@ -40,6 +67,7 @@
                              assoc :message (-> % .-target .-value))}]]
        [:input.button.is-primary
         {:type :submit
+         :on-click #(send-message! fields errors)
          :value "comment"}]])))
 
 (defn home
@@ -52,3 +80,4 @@
 (dom/render
  [home]
  (.getElementById js/document "content"))
+
