c244088b1d9378282cb102920df1dc95d818fb12
bjtj <bjtj10@gmail.com>
2021-09-25 22:34:26 +0900

    updated: web development with clojure - 4. introducing clojurescript -- build the ui with reagent

--- clojure/luminus/web-development-with-clojure/4.introducing-clojurescript/guestbook/src/cljs/guestbook/core.cljs
+++ clojure/luminus/web-development-with-clojure/4.introducing-clojurescript/guestbook/src/cljs/guestbook/core.cljs
@@ -1,5 +1,54 @@
-(ns guestbook.core)
+(ns guestbook.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as dom]))
 
-(-> (.getElementById js/document "content")
-    (.-innerHTML)
-    (set! "Hello Auto!"))
+(dom/render
+ [:h1 "Hello, Reagent"]
+ (.getElementById js/document "content"))
+
+(dom/render
+ [:div {:id "hello", :class "content"} [:h1 "Hello, Auto!"]]
+ (.getElementById js/document "content"))
+
+(dom/render
+ [:div#hello.content>h1 "Hello, Auto!!"]
+ (.getElementById js/document "content"))
+
+
+(defn message-form
+  ""
+  []
+  (let [fields (r/atom {})]
+    (fn []
+      [:div
+       [:p "Name: " (:name @fields)]
+       [:p "Message: " (:message @fields)]
+       [:div.field
+        [:label.label {:for :name} "Name"]
+        [:input.input
+         {:type :text
+          :name :name
+          :on-change #(swap! fields
+                             assoc :name (-> % .-target .-value))
+          :value (:name @fields)}]]
+       [:div.field
+        [:label.label {:for :message} "Message"]
+        [:textarea.textarea
+         {:name :message
+          :value (:message @fields)
+          :on-change #(swap! fields
+                             assoc :message (-> % .-target .-value))}]]
+       [:input.button.is-primary
+        {:type :submit
+         :value "comment"}]])))
+
+(defn home
+  ""
+  []
+  [:div.content>div.columns.is-centered>div.colums.is-two-thirds
+   [:div.columns>div.column
+    [message-form]]])
+
+(dom/render
+ [home]
+ (.getElementById js/document "content"))
