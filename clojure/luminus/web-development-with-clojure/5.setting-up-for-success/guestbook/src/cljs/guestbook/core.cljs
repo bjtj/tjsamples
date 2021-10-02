9c9633a44079375fabe33c6793393be6bc83586c
bjtj <bjtj10@gmail.com>
2021-10-02 23:58:01 +0900

    updated: web development with clojure - 5. setting up for success -- clojurescript development tools - shadow-cljs

--- clojure/luminus/web-development-with-clojure/5.setting-up-for-success/guestbook/src/cljs/guestbook/core.cljs
+++ clojure/luminus/web-development-with-clojure/5.setting-up-for-success/guestbook/src/cljs/guestbook/core.cljs
@@ -15,20 +15,41 @@
 (defn message-list
   ""
   [messages]
-  (println messages)
   [:ul.messages
    (for [{:keys [timestamp message name]} @messages]
      ^{:key timestamp}
      [:li
       [:time (.toLocaleString timestamp)]
       [:p message]
-      [:p " - " name]])])
+      [:p " @ " name]])])
 
 (defn errors-component
   ""
   [errors id]
   (when-let [error (id @errors)]
     [:div.notification.is-danger (string/join error)]))
+
+(defn send-message!
+  "Send Message!"
+  [fields errors]
+  (if-let [validation-errors (validate-message @fields)]
+    (reset! errors validation-errors)
+    (POST "/api/message"
+          {:format :json
+           :headers
+           {"Accept" "application/transit+json"
+            "x-csrf-token" (.-value (.getElementById js/document "token"))}
+           :params @fields
+           :handler (fn [_]
+                      (rf/dispatch
+                       [:message/add (-> @fields
+                                         (assoc :timestamp (js/Date.))
+                                         (update :name str " [CLIENT]"))])
+                      (reset! fields nil)
+                      (reset! errors nil))
+           :error-handler (fn [e]
+                            (.log js/console (str e))
+                            (reset! errors (-> e :response :errors)))})))
 
 (defn message-form
   ""
@@ -89,42 +110,38 @@
  (fn [db [_ message]]
    (update db :messages/list conj message)))
 
-(defn send-message!
-  "Send Message!"
-  [fields errors]
-  (if-let [validation-errors (validate-message @fields)]
-    (reset! errors validation-errors)
-    (POST "/api/message"
-          {:format :json
-           :headers
-           {"Accept" "application/transit+json"
-            "x-csrf-token" (.-value (.getElementById js/document "token"))}
-           :params @fields
-           :handler (fn [_]
-                      (rf/dispatch
-                       [:message/add (assoc @fields :timestamp (js/Date.))])
-                      (reset! fields nil)
-                      (reset! errors nil))
-           :error-handler (fn [e]
-                            (.log js/console (str e))
-                            (reset! errors (-> e :response :errors)))})))
-
 (defn home
   ""
   []
   (let [messages (rf/subscribe [:messages/list])]
-    (rf/dispatch [:app/initialize])
-    (get-messages)
     (fn []
-      (if @(rf/subscribe [:messages/loading?])
-        [:div/div.row>div.span12>h3
-         "Loading Messages..."]
-        [:div.content>div.columns.is-centered>div.colums.is-two-thirds
-         [:div.columns>div.column
-          [:h3 "Messages"]
-          [message-list messages]]
-         [:div.columnsL>div.column 
-          [message-form messages]]]))))
+      [:div.content>div.columns.is-centered>div.column.is-two-thirds
+       (if @(rf/subscribe [:messages/loading?])
+         [:h3 "Loading Messages..."]
+         [:div
+          [:div.columns>div.kcolumn
+           [:h3 "Messages"]
+           [message-list messages]]
+          [:div.columns>div.column
+           [message-form]]])])))
+
+(defn ^:dev/after-load mount-components
+  ""
+  []
+  (rf/clear-subscription-cache!)
+  (.log js/console "Mounting Components...")
+  (dom/render [#'home] (.getElementById js/document "content"))
+  (.log js/console "Components Mounted!"))
+
+(defn init!
+  ""
+  []
+  (.log js/console "Initializing App...")
+  (rf/dispatch [:app/initialize])
+  (get-messages)
+  (mount-components))
+
+(.log js/console "guestbook.core evaluated!")
 
 (dom/render
  [home]
