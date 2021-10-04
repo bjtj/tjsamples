c4b6ecd58e92e05b778958b179338700c898d131
bjtj <bjtj10@gmail.com>
2021-10-04 15:48:29 +0900

    added: web development with clojure - 6. planning our application

---
+++ clojure/luminus/web-development-with-clojure/6.planning-our-application/guestbook/src/clj/guestbook/auth.clj
@@ -0,0 +1,23 @@
+(ns guestbook.auth
+  (:require [buddy.hashers :as hashers]
+            [next.jdbc :as jdbc]
+            [guestbook.db.core :as db]))
+
+(defn create-user!
+  ""
+  [login password]
+  (jdbc/with-transaction [t-conn db/*db*]
+    (if-not (empty? (db/get-user-for-auth* t-conn {:login login}))
+      (throw (ex-info "User already exists!"
+                      {:guestbook/error-id ::duplicate-user
+                       :error "User already exists!"}))
+      (db/create-user!* t-conn
+                        {:login login
+                         :password (hashers/derive password)}))))
+
+(defn authenticate-user
+  ""
+  [login password]
+  (let [{hashed :password :as user} (db/get-user-for-auth* {:login login})]
+    (when (hashers/check password hashed)
+        (dissoc user :password))))
