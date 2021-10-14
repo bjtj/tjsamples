24f5a977224b66cb2afeb2c897cfd7cdb23374ab
bjtj <bjtj10@gmail.com>
2021-10-15 00:51:10 +0900

    added: spring boot - testing

---
+++ springboot/testing/todo/src/main/java/com/example/todo/ToDoRepository.java
@@ -0,0 +1,8 @@
+package com.example.todo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("toDoRepository")
+public interface ToDoRepository extends JpaRepository<ToDo, Long> {
+}
---
+++ springboot/testing/todo/src/test/java/com/example/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
