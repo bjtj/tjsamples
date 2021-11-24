9a0d54e950184ee087e3d5067d2817c87134b409
bjtj <bjtj10@gmail.com>
2021-11-24 23:21:08 +0900

    updated: swift data example

--- swift/data/main.swift
+++ swift/data/main.swift
@@ -1,23 +1,29 @@
 import Foundation
 
-let data = "hello world".data(using: .utf8)
+func main() {
+    guard let data = "hello world".data(using: .utf8) else {
+        exit(1)
+    }
 
-print("count: \(data!.count)")
+    print("count: \(data.count)")
 
-print("first: \(data![0])")
+    print("first: \(data[0])")
 
-print("loop")
-for d in data! {
-    print(d)
+    print("loop")
+    for d in data {
+        print(d)
+    }
+
+
+    print("\(data.subdata(in: 0..<2).map {"\($0)"} )")
+
+    print("\(data.subdata(in: 2..<7).map {"\($0)"} )")
+
+    print(String(data: data.subdata(in: 2..<7), encoding: .utf8)!)
+
+    print("line1\r\nline2\r\n".data(using: .utf8)!.range(of: "\r\n".data(using: .utf8)!))
+
+    print("line1\r\nline2\r\n".data(using: .utf8)!.range(of: "xx".data(using: .utf8)!))
 }
 
-
-print("\(data!.subdata(in: 0..<2).map {"\($0)"} )")
-
-print("\(data!.subdata(in: 2..<7).map {"\($0)"} )")
-
-print(String(data: data!.subdata(in: 2..<7), encoding: .utf8)!)
-
-print("line1\r\nline2\r\n".data(using: .utf8)!.range(of: "\r\n".data(using: .utf8)!))
-
-print("line1\r\nline2\r\n".data(using: .utf8)!.range(of: "xx".data(using: .utf8)!))
+main()
