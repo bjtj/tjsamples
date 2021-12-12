ef4bbc1581588ce356dd2080adfabb3b75c9179f
bjtj <bjtj10@gmail.com>
2021-12-12 12:39:19 +0900

    updated: batch function example

--- batch-script/function/main.bat
+++ batch-script/function/main.bat
@@ -1,18 +1,54 @@
 @echo off
 
-echo - call :foo 1 2 3 4
+rem Batch file call sub-batch file to pass n paramenters and return without use of file
+rem -----------------------------------------------------------------------------------
+rem <https://stackoverflow.com/a/11486159>
+
+setlocal
+
+echo ------------------------------------------------------------
+echo  `call :sum 1 2`
+echo ------------------------------------------------------------
+call :sum 1 2
+echo result is %errorlevel%
+
+echo ------------------------------------------------------------
+echo  `call :getmessage message`
+echo ------------------------------------------------------------
+call :getmessage message
+echo Message from :getmessage is '%message%'
+
+echo ------------------------------------------------------------
+echo  `call :foo 1 2 3 4`
+echo ------------------------------------------------------------
 call :foo 1 2 3 4
 
-echo - call :baz
+echo ------------------------------------------------------------
+echo  `call :baz`
+echo ------------------------------------------------------------
 call :baz
 
-echo - goto baz
+echo ------------------------------------------------------------
+echo  `goto baz`
+echo ------------------------------------------------------------
 goto baz
 
-echo - goto run (will not be called)
+echo ------------------------------------------------------------
+echo  `goto run (will not be called)`
+echo ------------------------------------------------------------
 goto run
 
 rem -----------------------------------
+
+:sum
+setlocal
+set /a "sum=%1 + %2"
+exit /b %sum%
+
+:getmessage
+set msg=HELLO
+set "%1=%msg%"
+exit /b
 
 :foo
 shift
