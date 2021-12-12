2944b045b13e6aa0aff564aa53c7e99a2212c5e8
bjtj <bjtj10@gmail.com>
2021-12-12 12:41:55 +0900

    added: batch script for loop example

--- batch-script/loop/for.bat
+++ batch-script/loop/for.bat
@@ -1,4 +1,27 @@
 @echo off
 setlocal
 
+echo ------------------------------------------------------------
+
 for /l %%x in (1, 1, 10) do echo %%x
+
+echo ------------------------------------------------------------
+
+for %%x in (a b) do echo %%x
+
+echo ------------------------------------------------------------
+
+set list=q w e r
+for %%x in (%list%) do echo %%x
+
+echo ------------------------------------------------------------
+
+for %%x in (%list%) do call :print %%x
+goto end
+
+:print
+echo - ITEM: %1
+goto :eof
+
+:end
+echo BYE~
