59abb44d4b597e9bf16a6a5ea6e6c3e85e1a8dd9
bjtj <bjtj10@gmail.com>
2021-12-12 13:32:33 +0900

    updated: batch script - path example (parent path)

--- batch-script/filesystem/path.bat
+++ batch-script/filesystem/path.bat
@@ -4,12 +4,32 @@ setlocal
 rem https://stackoverflow.com/a/5034119
 
 echo %0
+rem "path.bat "
+
+echo %~0
+rem path.bat 
+
 echo %~f0
+rem c:\tjsamples\batch-script\filesystem\path.bat
+
 echo %~dp0
+rem c:\tjsamples\batch-script\filesystem\
+
+for %%d in (%~dp0..) do set ParentDirectory=%%~fd
+echo %ParentDirectory%
+rem c:\tjsamples\batch-script
+
 echo %~d0
+rem c:
+
 echo %~p0
+rem \tjsamples\batch-script\filesystem\
 
 echo %~n0
+rem path
+
 echo %~x0
+rem .bat
 
 echo %~n0.txt
+rem path.txt
