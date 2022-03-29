47b0b3261069a1fd77d79e162f552ef8ce2c2a97
bjtj <bjtj10@gmail.com>
2022-03-29 21:18:03 +0900

    added: docker engine sdk example (go lang)

---
+++ docker/engine/sdk/go/README.md
@@ -0,0 +1,11 @@
+# Develop with Docker Engine SDKs #
+
+<https://docs.docker.com/engine/api/sdk/#sdk-and-api-quickstart>
+
+```shell
+$ go get github.com/docker/docker/client
+```
+
+```shell
+go run .
+```
---
+++ docker/engine/sdk/go/main.go
@@ -0,0 +1,56 @@
+// https://docs.docker.com/engine/api/sdk/#sdk-and-api-quickstart
+
+package main
+
+import (
+	"context"
+	"io"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+)
+
+func main() {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+
+	reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
+	if err != nil {
+		panic(err)
+	}
+	io.Copy(os.Stdout, reader)
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "alpine",
+		Cmd:   []string{"echo", "hello world"},
+	}, nil, nil, nil, "")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panic(err)
+		}
+	case <-statusCh:
+	}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		panic(err)
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+}
