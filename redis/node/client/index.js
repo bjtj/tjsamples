e565c4e7510bf0dae530e6ac122e02a1d63f1cc6
bjtj <bjtj10@gmail.com>
2022-04-03 17:10:45 +0900

    added: redis node example

---
+++ redis/node/client/index.js
@@ -0,0 +1,16 @@
+const { createClient } = require('redis');
+
+(async () => {
+    const client = createClient();
+
+    client.on('error', (err) => console.log('Redis Client Error', err));
+
+    await client.connect();
+
+    await client.set('key', 'value');
+    const value = await client.get('key');
+
+    console.log(`value of key: ${value}`);
+
+    client.disconnect();
+})();
