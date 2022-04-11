3214b14bf88e108c9d8faa8f1802dd9ff916e1ea
bjtj <bjtj10@gmail.com>
2022-04-11 16:05:11 +0900

    updatd: shell argument shift example

--- shell/args/args.sh
+++ shell/args/args.sh
@@ -4,16 +4,25 @@
 
 set -e
 
-echo "arguments: $@"
-echo "number of arguments: $#"
+echo "ARGUMENTS: '$@'"
+echo "NUMBER OF ARGUMENTS: $#"
 
 if [ -z "$1" ]; then
-    echo "no argument"
+    echo "[ERR] NO ARGUMENT..."
     exit 0
 fi
 
-echo "[shift]"
+i=0
+echo "[WHILE & SHIFT]"
 while [ -n "$1" ]; do
-    echo "$1"
+    echo "- ARG: $1"
     shift
+    i=$((i+1))
+    if [[ $i -eq 3 ]]
+    then break
+    fi
 done
+
+
+echo "REST ARGUMENTS: '$@'"
+echo "REST NUMBER OF ARGUMENTS: $#"
