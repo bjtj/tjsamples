bf55d06af128f58408506baac2de59e0b2f22109
bjtj <bjtj10@gmail.com>
2022-05-20 17:20:07 +0900

    updated: redis java (redisson) stream producer consumer

---
+++ redis/java/stream/src/kr/tj/sample/redis/stream/Producer.java
@@ -0,0 +1,28 @@
+package kr.tj.sample.redis.stream;
+
+import org.redisson.Redisson;
+import org.redisson.api.ObjectListener;
+import org.redisson.api.RStream;
+import org.redisson.api.RedissonClient;
+import org.redisson.api.StreamMessageId;
+import org.redisson.api.stream.StreamAddArgs;
+import org.redisson.api.stream.StreamReadArgs;
+import org.redisson.api.stream.StreamTrimArgs;
+
+public class Producer {
+
+  public static void main(String[] args) throws InterruptedException {
+    RedissonClient client = Redisson.create();
+    RStream<String, MyStreamData> stream = client.getStream("mystream");
+
+    int i = 0;
+    while (true) {
+      i++;
+      String id = "key-" + i;
+      MyStreamData data = new MyStreamData("val1", new byte[10]);
+      StreamMessageId msgid = stream.add(StreamAddArgs.entry(id, data));
+      System.out.println("ADD: " + id + " with " + msgid);
+      Thread.sleep(1000);
+    }
+  }
+}
