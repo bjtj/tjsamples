53d61de1207c46ff0869889a3807e9e7b43e61e3
bjtj <bjtj10@gmail.com>
2022-05-20 17:20:07 +0900

    updated: redis java (redisson) stream producer consumer

--- redis/java/stream/src/kr/tj/sample/redis/stream/Consumer.java
+++ redis/java/stream/src/kr/tj/sample/redis/stream/Consumer.java
@@ -4,23 +4,32 @@ import org.redisson.Redisson;
 import org.redisson.api.RStream;
 import org.redisson.api.RedissonClient;
 import org.redisson.api.StreamMessageId;
-import org.redisson.api.stream.StreamReadArgs;
 
 import java.util.Iterator;
 import java.util.Map;
+import org.redisson.api.stream.StreamReadArgs;
+import org.redisson.api.stream.StreamReadGroupArgs;
 
 public class Consumer {
     public static void main(String[] args) {
         RedissonClient client = Redisson.create();
         RStream<String, MyStreamData> stream = client.getStream("mystream");
 
+        StreamMessageId queryid = StreamMessageId.ALL;
+
         while (true) {
-            Map<StreamMessageId, Map<String, MyStreamData>> messages = stream.read(StreamReadArgs.greaterThan(StreamMessageId.MIN));
-            System.out.println(messages.size());
-            Iterator<StreamMessageId> keys = messages.keySet().iterator();
-            while (keys.hasNext()) {
-                StreamMessageId key = keys.next();
-                System.out.println(key);
+            Map<StreamMessageId, Map<String, MyStreamData>> messages = stream.read(
+              StreamReadArgs.greaterThan(queryid));
+            Iterator<StreamMessageId> ids = messages.keySet().iterator();
+            while (ids.hasNext()) {
+                StreamMessageId id = ids.next();
+                System.out.println(id);
+                Map<String, MyStreamData> map = messages.get(id);
+                for (String msgkey : map.keySet()) {
+                    MyStreamData data = map.get(msgkey);
+                    System.out.println(msgkey + " -- " + data.getKey() + ", " + data.getData().length);
+                }
+                queryid = id;
             }
         }
 
