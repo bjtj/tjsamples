f5b4e5b94b87fc80db7c4b7c62ffb5a80d0d9396
bjtj <bjtj10@gmail.com>
2022-07-08 13:40:44 +0900

    minor: python environment value type

--- python/environment/main.py
+++ python/environment/main.py
@@ -11,12 +11,16 @@ def main():
     print(sys.hexversion)
 
     print('python {}'.format(sys.version_info[0]))
-    
+
     for k, v in globals().items():
         print('{}: {}'.format(k, v))
 
     print(os.environ)
     print('PATH:', os.environ.get('PATH', ''))
+
+    print(os.getenv('MYVAL', 'default-my-value'))
+    print(int(os.getenv('MYVAL2', '123')))
+
 
 if __name__ == '__main__':
     main()
