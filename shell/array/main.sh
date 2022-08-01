e93c1f2bce6b1c2b59915a82c48e771387423a74
bjtj <bjtj10@gmail.com>
2022-08-01 13:17:02 +0900

    minor: shell array length

--- shell/array/main.sh
+++ shell/array/main.sh
@@ -2,12 +2,15 @@
 
 ARR=(1 2 3)
 
-echo "---"
+echo "ARR length = ${#ARR[@]}"
+echo "ARR = ${ARR[@]}"
+
+echo "= ITERATE ="
 for i in ${ARR[@]}; do
     echo $i
 done
 
-echo "---"
+echo "= INDEX ="
 echo ${ARR[0]}
 echo ${ARR[1]}
 echo ${ARR[2]}
