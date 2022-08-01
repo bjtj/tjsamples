7ae31022aebfad0e4bd58a9a48355897ccc83967
bjtj <bjtj10@gmail.com>
2022-08-01 13:35:51 +0900

    updated: shell array - convert string to array

--- shell/array/main.sh
+++ shell/array/main.sh
@@ -14,3 +14,17 @@ echo "= INDEX ="
 echo ${ARR[0]}
 echo ${ARR[1]}
 echo ${ARR[2]}
+
+
+echo "= ITERATE STRING ELEMENTS ="
+ELEMS="1 2 3"
+for i in $ELEMS; do
+    echo $i
+done
+
+echo "= INDEX STRING ELEMENTS ="
+ELEMS_ARR=($ELEMS)
+
+echo ${ELEMS_ARR[0]}
+echo ${ELEMS_ARR[1]}
+echo ${ELEMS_ARR[2]}
