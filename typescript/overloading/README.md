aea8845798a3b9ccd4efbdaad6c04ae4ec171d4d
bjtj <bjtj10@gmail.com>
2022-08-04 11:42:56 +0900

    updated: typescript overloading example

---
+++ typescript/overloading/README.md
@@ -0,0 +1,10 @@
+# TypeScript Function Overloading #
+
+<https://howtodoinjava.com/typescript/function-overloading/>
+
+## Run ##
+
+```bash
+npx tsc index.ts 
+node index.js
+```
--- typescript/overloading/index.ts
+++ typescript/overloading/index.ts
@@ -14,6 +14,10 @@ function add(first: number, second: number, third?: number, fourth?: number): nu
   }
 }
 
+console.log('');
+console.log('[CALL add]');
+console.log('---');
+
 const r1 = add(1, 2, 3);
 const r2 = add(1, 2);
 // const r3 = add(1, 2, 3, 4); // <--- ERROR: `index.ts(17,25): error TS2554: Expected 2-3 arguments, but got 4.`
@@ -45,6 +49,53 @@ function findEmployees(name: string, age?: number, location?: string): Employee[
 	return employee;
 }
 
+console.log('');
+console.log('[CALL findEmployees]');
+console.log('---');
+
 findEmployees('John', 10);
 // findEmployees('My John', 'my location'); // <--- ERROR: `index.ts(49,26): error TS2345: Argument of type 'string' is not assignable to parameter of type 'number'.`
 findEmployees('My John', 12, 'my location');
+
+
+// ----
+
+// MY EXAMPLE
+
+// function foox(name: string): void;
+// function foox(name: string, opts?: string): void {
+//     console.log(`name: ${name}, opts: ${opts}`)
+// }
+
+
+function foo(name: string, callback: () => void): void;
+function foo(name: string, _opts: any, _callback: () => void): void;
+function foo(name: string, _opts?: any, _callback?: () => void): void {
+    console.log(`NAME: ${name}`);
+		let opts = _opts;
+		let callback = _callback;
+		if (!_callback) {
+				opts = null;
+				callback = _opts;
+		}
+		if (opts) {
+				console.log(`OPTS => ${JSON.stringify(opts)}`);
+				callback();
+		} else {
+				callback();
+		}				
+		
+}
+
+
+console.log('');
+console.log('[CALL foo]');
+console.log('---');
+
+foo('my name', () => {
+		console.log('done');
+});
+
+foo('my name', { key: 'value' }, () => {
+		console.log('done');
+});
