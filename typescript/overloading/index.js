937a1411bdf8f9287d038e5fb4c25f9d9779a096
bjtj <bjtj10@gmail.com>
2022-08-04 11:42:56 +0900

    updated: typescript overloading example

--- typescript/overloading/index.js
+++ typescript/overloading/index.js
@@ -9,6 +9,9 @@ function add(first, second, third, fourth) {
         return first + second;
     }
 }
+console.log('');
+console.log('[CALL add]');
+console.log('---');
 var r1 = add(1, 2, 3);
 var r2 = add(1, 2);
 // const r3 = add(1, 2, 3, 4); // <--- ERROR: `index.ts(17,25): error TS2554: Expected 2-3 arguments, but got 4.`
@@ -33,6 +36,34 @@ function findEmployees(name, age, location) {
     }
     return employee;
 }
+console.log('');
+console.log('[CALL findEmployees]');
+console.log('---');
 findEmployees('John', 10);
 // findEmployees('My John', 'my location'); // <--- ERROR: `index.ts(49,26): error TS2345: Argument of type 'string' is not assignable to parameter of type 'number'.`
 findEmployees('My John', 12, 'my location');
+function foo(name, _opts, _callback) {
+    console.log("NAME: ".concat(name));
+    var opts = _opts;
+    var callback = _callback;
+    if (!_callback) {
+        opts = null;
+        callback = _opts;
+    }
+    if (opts) {
+        console.log("OPTS => ".concat(JSON.stringify(opts)));
+        callback();
+    }
+    else {
+        callback();
+    }
+}
+console.log('');
+console.log('[CALL foo]');
+console.log('---');
+foo('my name', function () {
+    console.log('done');
+});
+foo('my name', { key: 'value' }, function () {
+    console.log('done');
+});
