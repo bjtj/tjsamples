6abe9720aa84ece73930f66532fc8042eac0d783
bjtj <bjtj10@gmail.com>
2022-08-17 10:19:46 +0900

    updated: shell to array example

--- shell/array/main.sh
+++ shell/array/main.sh
@@ -28,3 +28,23 @@ ELEMS_ARR=($ELEMS)
 echo ${ELEMS_ARR[0]}
 echo ${ELEMS_ARR[1]}
 echo ${ELEMS_ARR[2]}
+
+# TO ARRAY
+# --------
+#  - https://stackoverflow.com/a/10586169
+
+echo "============== TO ARRAY 1 =============="
+ELEMENTS="1 2 3"
+IFS=', ' read -r -a array <<< "$ELEMENTS"
+echo ${array[@]}
+echo ${array[0]}
+echo ${array[1]}
+echo ${array[2]}
+
+echo "============== TO ARRAY 2 =============="
+TOARR=($ELEMENTS)
+echo ${TOARR[@]}
+echo ${TOARR[0]}
+echo ${TOARR[1]}
+echo ${TOARR[2]}
+
