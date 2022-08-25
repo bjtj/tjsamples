e6fe96e1d25137684b0e2d6d17c060944a4aa435
bjtj <bjtj10@gmail.com>
2022-08-25 23:41:58 +0900

    added: rxjs example

---
+++ reactive/rxjs/example/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => {
+});
