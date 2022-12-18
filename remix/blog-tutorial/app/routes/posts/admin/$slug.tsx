e7afab2c54717a97473c09887832ca26d3cd99d6
bjtj <bjtj10@gmail.com>
2022-12-18 21:56:00 +0900

    minor: remix

--- remix/blog-tutorial/app/routes/posts/admin/$slug.tsx
+++ remix/blog-tutorial/app/routes/posts/admin/$slug.tsx
@@ -17,20 +17,6 @@ export const loader = async ({ params }: LoaderArgs) => {
 
   return json({ post });
 };
-/* 
- * export default function PostSlug() {
- *   const { post, html } = useLoaderData<typeof loader>();
- *   return (
- *     <main className="mx-auto max-w-4xl">
- *       <h1 className="my-6 border-b-2 text-center text-3xl">
- *         {post.title}
- *       </h1>
- *       <div dangerouslySetInnerHTML={{ __html: html }} />
- *     </main>
- *   );
- * }
- *  */
-
 
 export const action = async ({ request }: ActionArgs) => {
 
