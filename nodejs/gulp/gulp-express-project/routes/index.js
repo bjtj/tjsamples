3478f033aa8c94437a091b8287077eb58eebb985
bjtj <bjtj10@gmail.com>
2023-07-01 13:19:20 +0900

    added: gulp + express example

---
+++ nodejs/gulp/gulp-express-project/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/hello', function(req, res, next) {
+  res.send('hello world');
+});
+
+module.exports = router;
