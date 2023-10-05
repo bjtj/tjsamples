3c0e9f6c4a552a10c2b9993b92ae47bbd79e11a9
bjtj <bjtj10@gmail.com>
2023-10-05 13:03:55 +0900

    Minor update: javascript array pop example

--- web/javascript/array/main.js
+++ web/javascript/array/main.js
@@ -9,3 +9,11 @@ console.log([1,2,3].slice(1, 1)); // []
 console.log([1,2,3].slice(1, 2)); // [ 2 ]
 console.log([1,2,3].slice(1, 6)); // [ 2, 3 ]
 console.log([1,2,3].slice(1));	  // [ 2, 3 ]
+
+
+
+let arr = [1,2,3,4,5];
+arr.push(6);
+console.log(arr);                  // [ 1, 2, 3, 4, 5, 6 ]
+console.log(arr.pop());            // 6
+console.log(arr);                  // [ 1, 2, 3, 4, 5 ]
