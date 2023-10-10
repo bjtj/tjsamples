f37ad29b35f799e2baeedccbf188fdd3c1b3cf0d
bjtj <bjtj10@gmail.com>
2023-10-10 11:03:00 +0900

    Minor: python logging - root logger

--- python/logging/root/main.py
+++ python/logging/root/main.py
@@ -1,13 +1,22 @@
 import logging
 from logging import FileHandler
 
-root_logger = logging.getLogger()
-handler = FileHandler('./.log/out.log')
-handler.setFormatter(logging.Formatter('%(asctime)s %(name)s --- %(levelname)s : %(message)s'))
-root_logger.addHandler(handler)
+def _get_file_handler():
+    handler = FileHandler('./.log/out.log')
+    handler.setFormatter(logging.Formatter('%(asctime)s %(name)s --- %(levelname)s : %(message)s'))
+    return handler
+
+def _set_root_logger():
+    root_logger = logging.getLogger()
+    handler = _get_file_handler()
+    root_logger.addHandler(handler)
 
 
 def main():
+
+    logging.basicConfig(handlers=[_get_file_handler()])
+    _set_root_logger()
+    
     logger = logging.getLogger('log1')
     logger.setLevel(logging.DEBUG)
     logger.debug('hello')
