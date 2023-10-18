544b70a7a7a782b0505a3bdfc15da8e382d00ff1
bjtj <bjtj10@gmail.com>
2023-10-18 13:49:17 +0900

    Added: dotnet signal example

---
+++ dotnet/signal/example/README.md
@@ -0,0 +1,3 @@
+# ConsoleCancelEventHandler Delegate #
+
+<https://learn.microsoft.com/en-us/dotnet/api/system.consolecanceleventhandler?view=net-7.0>
---
+++ dotnet/signal/myapp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+
+  static void Main(string[] args)
+    {
+      ConsoleKeyInfo cki;
+
+      Console.CancelKeyPress += new ConsoleCancelEventHandler(handler);
+
+      Console.WriteLine("PROGRAM::START");
+      Thread.Sleep(10000);
+      Console.WriteLine("PROGRAM::DONE");
+    }
+
+  public static void handler(object sender, ConsoleCancelEventArgs args)
+    {
+      Console.WriteLine($"KEY: {args.SpecialKey}");
+      Console.WriteLine($" - Cancel?: {args.Cancel}");
+
+      // prevent the process from termiating
+      args.Cancel = true;
+    }
+}
