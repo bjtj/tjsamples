7d9af969f5e7c26b8001c388db2732cccd871be6
bjtj <bjtj10@gmail.com>
2023-10-19 13:46:56 +0900

    Added: redis - dotnet example

---
+++ redis/dotnet/READMD.md
@@ -0,0 +1,19 @@
+# C#/.NET guide #
+
+<https://redis.io/docs/clients/dotnet/>
+
+```shell
+dotnet add package NRedisStack
+```
+
+
+# NRedisStack #
+
+<https://github.com/redis/NRedisStack>
+
+
+
+# StackExchange.Redis #
+
+<https://github.com/StackExchange/StackExchange.Redis>
+
---
+++ redis/dotnet/example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using NRedisStack;
+using NRedisStack.RedisStackCommands;
+using StackExchange.Redis;
+
+//...
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("127.0.0.1");
+// IDatabase db = redis.GetDatabase();
+
+ISubscriber sub = redis.GetSubscriber();
+
+sub.Subscribe("messages", (channel, message) => {
+  Console.WriteLine((string)message);
+});
+
+sub.Publish("messages", "hello");
+
+Thread.Sleep(10000);
