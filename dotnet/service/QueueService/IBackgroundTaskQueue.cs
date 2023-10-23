79fe93f94b22dbafceb32c87dd4ff2bb2aca263d
bjtj <bjtj10@gmail.com>
2023-10-23 19:39:44 +0900

    Added: dotnet queue service example

---
+++ dotnet/service/QueueService/IBackgroundTaskQueue.cs
@@ -0,0 +1,7 @@
+namespace App.QueueService;
+
+public interface IBackgroundTaskQueue
+{
+  ValueTask QueueBackgroundWorkItemAsync(Func<CancellationToken, ValueTask> workItem);
+  ValueTask<Func<CancellationToken, ValueTask>> DequeueAsync(CancellationToken cancellationToken);
+}
---
+++ dotnet/service/QueueService/README.md
@@ -0,0 +1,3 @@
+# Create a Queue Service #
+
+<https://learn.microsoft.com/en-us/dotnet/core/extensions/queue-service?pivots=dotnet-7-0>
