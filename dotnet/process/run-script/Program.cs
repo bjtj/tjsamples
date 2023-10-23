1696d9bebd78083f9b079eaba5ef73da9ef283d9
bjtj <bjtj10@gmail.com>
2023-10-23 18:58:04 +0900

    Added: dotnet process run script example

---
+++ dotnet/process/run-script/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+
+var ps1File = @"hello.ps1";
+
+var startInfo = new ProcessStartInfo()
+{
+  FileName = "powershell.exe",
+  Arguments = $"-NoProfile -ExecutionPolicy ByPass -File \"{ps1File}\"",
+  UseShellExecute = false
+};
+
+var proc = Process.Start(startInfo);
+
+if (proc != null) {
+  proc.WaitForExit();
+  Console.WriteLine($"Result: {proc.ExitCode}");
+}
+
