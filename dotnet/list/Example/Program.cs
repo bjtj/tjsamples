5c3967b024dabae533ccab9e4dae2765667e79a7
bjtj <bjtj10@gmail.com>
2023-10-24 15:44:33 +0900

    Added: dotnet list example

---
+++ dotnet/list/Example/Program.cs
@@ -0,0 +1,31 @@
+﻿
+List<string> list = new List<string>();
+
+
+list.Add("Hello");
+list.Add("World");
+
+
+foreach (string x in list)
+{
+  Console.WriteLine(x);
+}
+
+
+Console.WriteLine(String.Join(", ", list));
+
+Console.WriteLine($"Count: {list.Count}");
+
+list.Clear();
+
+Console.WriteLine($"Count: {list.Count}");
+
+
+list.Add("A");
+list.Add("B");
+list.Add("C");
+list.Add("D");
+
+list.Remove("B");
+
+Console.WriteLine(String.Join(", ", list));
---
+++ dotnet/list/Example/README.md
@@ -0,0 +1,3 @@
+# List #
+
+<https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1?view=net-7.0>
