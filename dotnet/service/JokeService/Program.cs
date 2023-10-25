68f3839ca1d0d4d679058a464fda50e609bde754
bjtj <bjtj10@gmail.com>
2023-10-25 16:10:17 +0900

    Updated: dotnet - windows service installer example

--- dotnet/service/JokeService/Program.cs
+++ dotnet/service/JokeService/Program.cs
@@ -2,6 +2,39 @@ using App.WindowsService;
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Logging.EventLog;
 
+using CliWrap;
+
+const string ServiceName = ".NET Joke Service";
+
+if (args is { Length: 1 })
+{
+	try
+	{
+		string executablePath = Path.Combine(AppContext.BaseDirectory, "JokeService.exe");
+		if (args[0] is "/Install")
+		{
+			await Cli.Wrap("sc")
+				.WithArguments(new[] { "create", ServiceName, $"binPath={executablePath}", "start=auto" })
+				.ExecuteAsync();
+		}
+		else if (args[0] is "/Uninstall")
+		{
+			await Cli.Wrap("sc")
+				.WithArguments(new[] { "stop", ServiceName })
+				.ExecuteAsync();
+
+			await Cli.Wrap("sc")
+				.WithArguments(new[] { "delete", ServiceName })
+				.ExecuteAsync();
+		}
+	}
+	catch (Exception ex)
+	{
+		Console.WriteLine(ex);
+	}
+	return;
+}
+
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddWindowsService(options =>
   {
