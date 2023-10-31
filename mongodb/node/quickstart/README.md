228b5889696a6960c6892385d878e1900326463b
bjtj <bjtj10@gmail.com>
2023-10-31 14:46:32 +0900

    Added: mongodb node quickstart

---
+++ mongodb/node/quickstart/README.md
@@ -0,0 +1,8 @@
+# Download and Install #
+
+<https://www.mongodb.com/docs/drivers/node/current/quick-start/download-and-install/>
+
+
+# MongoDB Sample Dataset #
+
+<https://github.com/neelabalan/mongodb-sample-dataset>
---
+++ mongodb/node/quickstart/index.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your connection string.
+const uri = "mongodb://127.0.0.1:27017";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
