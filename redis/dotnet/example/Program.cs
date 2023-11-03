b52a7b8fc26f4b1c0de06f427ad34d931545981e
bjtj <bjtj10@gmail.com>
2023-11-03 15:24:45 +0900

    Updated: redis dotnet example

--- redis/dotnet/example/Program.cs
+++ redis/dotnet/example/Program.cs
@@ -5,15 +5,16 @@ using StackExchange.Redis;
 
 //...
 
-ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("127.0.0.1");
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
 // IDatabase db = redis.GetDatabase();
 
 ISubscriber sub = redis.GetSubscriber();
 
 sub.Subscribe("messages", (channel, message) => {
-  Console.WriteLine((string)message);
+  Console.WriteLine("RECV: " + message.ToString());
 });
 
+Console.WriteLine("SEND: hello");
 sub.Publish("messages", "hello");
 
-Thread.Sleep(10000);
+Thread.Sleep(3000);
