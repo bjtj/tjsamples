b31bb247fda6d3ac702dbd3f8896438f82d79ff6
bjtj <bjtj10@gmail.com>
2023-11-03 15:24:15 +0900

    Added: dotnet envs example

---
+++ dotnet/envs/envs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+
+class Program {
+  static void Main(string[] args)
+    {
+      foreach(DictionaryEntry e in System.Environment.GetEnvironmentVariables())
+      {
+        Console.WriteLine(e.Key  + ":" + e.Value);
+      }
+
+      for (var i = 0; i < args.Length; ++i) {
+        Console.WriteLine("[" + i + "] " + args[i]);
+      }
+    }
+
+}
+
+
