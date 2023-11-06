eb2dc78e9204bd854b518e7c3eaa7b43706ac1d8
bjtj <bjtj10@gmail.com>
2023-11-06 16:05:56 +0900

    Updated: dotent environment example

--- dotnet/envs/envs/Program.cs
+++ dotnet/envs/envs/Program.cs
@@ -1,19 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.IO;
+using System.Collections;
 
 
 class Program {
   static void Main(string[] args)
     {
-      foreach(DictionaryEntry e in System.Environment.GetEnvironmentVariables())
-      {
-        Console.WriteLine(e.Key  + ":" + e.Value);
-      }
 
       for (var i = 0; i < args.Length; ++i) {
         Console.WriteLine("[" + i + "] " + args[i]);
       }
+
+      string[] xargs = Environment.GetCommandLineArgs();
+      Console.WriteLine("Args: {0}", string.Join(", ", xargs));
+
+      
+      foreach(DictionaryEntry e in System.Environment.GetEnvironmentVariables())
+      {
+        Console.WriteLine(e.Key  + ":" + e.Value);
+      }
     }
-
 }
-
-
