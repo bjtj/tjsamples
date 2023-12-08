22d039465c3e12ca0b246cee1d71d8a49dc948c6
bjtj <bjtj10@gmail.com>
2023-12-08 14:21:15 +0900

    Added: dotnet - named semaphore example

---
+++ dotnet/ipc/named_semaphore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+
+
+Semaphore sem = new Semaphore(initialCount: 1, maximumCount: 1, name: "Global\\MySemaphore");
+
+bool ret = sem.WaitOne(0);
+System.Console.WriteLine($"WaitOne(0) - {ret}");
+if (!ret)
+{
+    System.Console.WriteLine("Another instance of this application is already running.");
+    return;
+}
+
+string? input = System.Console.ReadLine();
+
+System.Console.WriteLine($"User Input - {input}");
+
+sem.Release();
