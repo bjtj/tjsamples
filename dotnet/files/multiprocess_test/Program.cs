a7e3ed18bfc807aa2b560023b1e5a3baa71cd1d6
bjtj <bjtj10@gmail.com>
2023-12-08 14:19:24 +0900

    Added: dotnet - file multiple access example

---
+++ dotnet/files/multiprocess_test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+string lockFileName = ".lock";
+
+if (File.Exists(lockFileName)) {
+    File.Delete(lockFileName);
+}
+
+FileStream stream = File.Create(lockFileName);
+
+Thread.Sleep(10000);
+
+stream.Close();
