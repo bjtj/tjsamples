9e88b3e334151e1dc3bf2f16279be4ea65175f12
bjtj <bjtj10@gmail.com>
2023-12-08 14:14:36 +0900

    Added: dotnet file list example

---
+++ dotnet/files/list/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+	public static void Main() {
+
+		System.Console.WriteLine("=== Files ===");
+		
+		var files = Directory.GetFiles(".");
+		foreach (var file in files) {
+			System.Console.WriteLine($"{file}");
+		}
+
+		System.Console.WriteLine("=== Directories ===");
+
+		var dirs = Directory.GetDirectories(".");
+		foreach (var dir in dirs) {
+			System.Console.WriteLine($"{dir}");
+		}
+	}
+}
