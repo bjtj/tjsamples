2c57735b718f6c353535509973972946170579cf
bjtj <bjtj10@gmail.com>
2024-01-08 15:44:12 +0900

    Added: matplotlib example

---
+++ matplotlib/quick-start/README.md
@@ -0,0 +1,11 @@
+# Matplotlib 3.8.2 documentation #
+
+<https://matplotlib.org/stable/>
+
+``` shell
+pip install matplotlib
+```
+
+## Quick start guide ##
+
+<https://matplotlib.org/stable/users/explain/quick_start.html#a-simple-example>
---
+++ matplotlib/quick-start/main.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib as mpl
+
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
+plt.show()
