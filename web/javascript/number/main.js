b17a3793b5c1c9b9003d6126534c4b8df01a7340
bjtj <bjtj10@gmail.com>
2024-01-18 18:26:40 +0900

    updated: web javascript number example

--- web/javascript/number/main.js
+++ web/javascript/number/main.js
@@ -1,4 +1,6 @@
-const comma = number => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+// const comma = number => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+const comma = number => number.toLocaleString();
 
 console.log(comma(1));
 console.log(comma(10));
@@ -60,3 +62,21 @@ console.log(commaHuman(10000000000000));
 console.log(commaHuman(100000000000000));
 console.log(commaHuman(1000000000000000));
 console.log(commaHuman(2637820981240193));
+
+console.log(parseInt('0'));              // 0
+console.log(parseInt('1'));              // 1
+console.log(parseInt('2'));              // 2
+console.log(parseInt('020'));            // 20
+console.log(parseInt('090'));            // 90
+
+console.log(parseInt('0abc'));           // 0
+console.log(parseInt('1abc'));           // 1
+console.log(parseInt('2abc'));           // 2
+console.log(parseInt('020abc'));         // 20
+console.log(parseInt('090abc'));         // 90
+
+console.log(parseInt('abc0abc'));        // NaN
+console.log(parseInt('abc1abc'));        // NaN
+console.log(parseInt('abc2abc'));        // NaN
+console.log(parseInt('abc020abc'));      // NaN
+console.log(parseInt('abc090abc'));      // NaN
