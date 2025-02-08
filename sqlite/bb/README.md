52e677ef766bca21bc042ab8aeccce143dcd7519
bjtj <bjtj10@gmail.com>
2025-02-08 10:05:05 +0900

    Added: sqlite babashka example

---
+++ sqlite/bb/README.md
@@ -0,0 +1,4 @@
+# pod-babashka-go-sqlite3 #
+
+<https://github.com/babashka/pod-babashka-go-sqlite3>
+
---
+++ sqlite/bb/main.clj
@@ -0,0 +1,19 @@
+#!/usr/bin/env bb
+
+(require '[pod.babashka.go-sqlite3 :as sqlite]
+         '[cheshire.core :as json])
+
+(def db "foo.db")
+
+(sqlite/execute!
+ db
+ ["CREATE TABLE employee (id INTEGER PRIMARY KEY AUTOINCREMENT,
+name VARCHAR(64),
+age INT,
+created DATETIME)"])
+
+(sqlite/execute! db ["INSERT INTO employee (name, age, created) VALUES (?, ?, datetime('now'))"
+                     "Steve" 24])
+
+(let [results (sqlite/query db ["SELECT * FROM employee"])]
+  (prn :employees results))
