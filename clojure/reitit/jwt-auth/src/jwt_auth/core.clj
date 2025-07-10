0fd44b33fec0a9def2b42f1cc3d77b0e4d983ac8
bjtj <bjtj10@gmail.com>
2025-07-11 00:32:33 +0900

    Added: reitit + buddy - jwt auth example

---
+++ clojure/reitit/jwt-auth/src/jwt_auth/core.clj
@@ -0,0 +1,113 @@
+(ns jwt-auth.core
+  (:gen-class)
+  (:require [reitit.ring :as ring]
+            [reitit.ring.middleware.parameters :refer [parameters-middleware]]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.util.response :as res]
+            [buddy.auth :refer [authenticated?]]
+            [buddy.auth.backends.token :refer [jws-backend]]
+            [buddy.auth.middleware :refer [wrap-authentication wrap-authorization]]
+            [buddy.sign.jwt :as jwt]
+            [clojure.data.json :as json]))
+
+(def secret "my-super-secret")
+
+(defn generate-token
+  "Sample JWT token generator (for testing)"
+  [user-id]
+  (jwt/sign {:user-id user-id} secret))
+
+;;; Auth backend for buddy
+(def auth-backend
+  (jws-backend {:secret secret
+                :options {:alg :hs256}}))
+
+(defn wrap-auth
+  "middleware to require authentication"
+  [handler]
+  (wrap-authentication handler auth-backend))
+
+(defn wrap-authz
+  "middleware to require authorization"
+  [handler]
+  (wrap-authorization handler auth-backend))
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    [["/" {:get (fn [_] (-> (str "<div><a href=\"login\">login</a></div>"
+                                 "<div>"
+                                 "  <input name=\"token\" placeholder=\"token...\" />"
+                                 "  <button onclick=\"fetch('/private', {method: 'get', headers: {'Authorization': `Token ${document.querySelector('[name=token]').value}`}}).then(resp => resp.text()).then(text => document.querySelector('#result').innerText = text)\">private api</button></div>"
+                                 "<div id=\"result\"></div>")
+                            (res/response)
+                            (res/content-type "text/html")))}]
+     ["/public" {:get (fn [_] (-> "Public route"
+                                  (res/response)
+                                  (res/content-type "text/html")))}]
+     ["/login"
+      {:get (fn [_]
+              (-> (str "<div><a href=\"/\">home</a></div>"
+                       "<form method=\"post\">"
+                       "<input name=\"username\" placeholder=\"username...\"/>"
+                       "<button>submit</button>"
+                       "</form>")
+                  (res/response)
+                  (res/content-type "text/html")))
+       :post (fn [{:keys [params]}]
+               ;; just a dummy login for demonstration
+               (let [username (params "username")
+                     token (generate-token username)]
+                 (-> (json/write-str {:username username :token token})
+                     (res/response)
+                     (res/content-type "application/json"))))}]
+     ["/private"
+      {:get {:middleware [[wrap-auth] [wrap-authz]]
+             :handler (fn [{:keys [identity] :as req}]
+                        ;; check header --> Authorization: Token <token>
+                        (if (authenticated? req)
+                          (-> (pr-str :authorized :identity identity)
+                              (res/response)
+                              (res/content-type "text/plain"))
+                          (-> (pr-str :unauthorized)
+                              (res/response)
+                              (res/status 401))))}}]])
+   (ring/create-default-handler)
+   {:middleware [parameters-middleware]}))
+
+(defonce server (atom nil))
+
+(defn stop
+  "stop server"
+  []
+  (when-let [s @server]
+    (prn :stop-server!)
+    (.stop s))
+  (reset! server nil))
+
+(defn start
+  "start server"
+  []
+  (if-let [s @server]
+    (prn :already-started s)
+    (reset! server (jetty/run-jetty #'app {:port 3000 :join? false}))))
+
+(defn restart
+  "restart server"
+  []
+  (stop)
+  (start))
+
+(comment
+  (json/write-str {:type "hello"})
+  (stop)
+  (start)
+  (restart)
+
+  ;; -- end of comment --
+  )
+
+(defn -main
+  [& args]
+  (jetty/run-jetty #'app {:port 3000 :join? true}))
---
+++ clojure/reitit/jwt-auth/test/jwt_auth/jwt_auth_test.clj
@@ -0,0 +1,7 @@
+(ns jwt-auth.jwt-auth-test
+  (:require [clojure.test :refer :all]
+            [jwt-auth.jwt-auth :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
